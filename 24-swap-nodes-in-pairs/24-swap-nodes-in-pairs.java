9146e749af6a791cf2ae9c78847a83035a009422
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-02-16 12:05:59 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (13.70%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -10,11 +10,19 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        if ((head == null) || (head.next == null))
+        if (head == null || head.next == null)
             return head;
-        ListNode n = head.next;
-        head.next = swapPairs(head.next.next);
-        n.next = head;
-        return n;
+        ListNode cur = head;
+        ListNode newNode = head.next;
+        while (cur != null && cur.next != null) {
+            ListNode tmp = cur;
+            cur = cur.next;
+            tmp.next = cur.next;
+            cur.next = tmp;
+            cur = tmp.next;
+            if (cur != null && cur.next != null)
+                tmp.next = cur.next;
+        }
+        return newNode;
     }
 }
