277c67aec5a0bfc0f009f4889ff16e1a73e1faf3
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-02-19 13:38:59 +0530

    Time: 17 ms (32.23%), Space: 7.2 MB (94.39%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        if (s.length() != t.length())
-            return false;
-        int n = s.size();
-        int count[26] = {0};
-        for (int i = 0; i < n; i++) {
-            count[s[i] - 'a']++;
-            count[t[i] - 'a']--;
-        }
-        for (int i = 0; i < 26; i++) 
-            if (count[i])
-                return false;
-        return true;
+    bool isAnagram(string s, string t) { 
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t; 
     }
 };
