813a91e0f8c9c14d5be7acabe228aa21e3e00077
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-02-20 11:40:24 +0530

    Time: 32 ms (55.48%), Space: 11.3 MB (89.47%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,14 +1,15 @@
 class Solution {
-    public int removeCoveredIntervals(int[][] A) {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& A) {
         int res = 0, left = -1, right = -1;
-        Arrays.sort(A, (a,b) -> a[0] - b[0]);
-        for (int[] v : A) {
-            if (v[0] > left && v[1] > right){
+        sort(A.begin(), A.end());
+        for (auto& v : A) {
+            if (v[0] > left && v[1] > right) {
                 left = v[0];
-                res++;
+                ++res;
             }
-            right = Math.max(right, v[1]);
+            right = max(right, v[1]);
         }
         return res;
     }
-}
+};
