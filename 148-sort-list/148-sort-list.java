cd7fea15c76301386257f14daec391f08f15ac4a
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-02-24 13:16:30 +0530

    Time: 5 ms (99.78%), Space: 50.1 MB (78.31%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,20 +10,40 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        while (head != null) {
-            list.add(head.val);
-            head = head.next;
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+    
+    ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
         }
-        
-        Collections.sort(list);
-        ListNode dummy = new ListNode(0);
-        ListNode ret = dummy;
-        for (int i = 0; i < list.size(); i++) {
-            ListNode newNode = new ListNode(list.get(i));
-            ret.next = newNode;
-            ret = ret.next;
-        }
+        tail.next = (list1 != null) ? list1 : list2;
         return dummy.next;
+    }
+    
+    ListNode getMid(ListNode head) {
+        ListNode midPrev = null;
+        while (head != null && head.next != null) {
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
+        }
+        ListNode mid = midPrev.next;
+        midPrev.next = null;
+        return mid;
     }
 }
