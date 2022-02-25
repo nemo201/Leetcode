bfa2c6fa4cf10cacf9fbe595bed563956413f18c
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-02-25 13:33:02 +0530

    Time: 5 ms (15.77%), Space: 6.2 MB (35.61%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {
-    for(auto& w : version1) if (w == '.') w=' ';
-    for(auto& w : version2) if (w == '.') w=' ';
-    istringstream s1(version1), s2(version2);
-    while(1) {
-        int n1,n2;
-        if (!(s1 >> n1) ) 
-            n1 = 0;
-        if (!(s2 >> n2) ) 
-            n2 = 0;
-        if (!s1 && !s2) 
-            return 0;
-        if (n1<n2) 
-            return -1;
-        if (n1>n2) 
-            return 1;
+    int compareVersion(string v1, string v2) {
+        for (auto& v : v1)
+            if (v == '.')
+                v = ' ';
+        for (auto& v : v2)
+            if (v == '.')
+                v = ' ';
+        
+        istringstream s1(v1), s2(v2);
+        
+        while (1) {
+            int n, m;
+            if (!(s1 >> n))
+                n = 0;
+            if (!(s2 >> m))
+                m = 0;
+            if (!s1 && !s2)
+                return 0;
+            if (n > m)
+                return 1;
+            if (m > n)
+                return -1;
+        }
     }
-}
 };
