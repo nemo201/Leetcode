c49b55d85a933459071217f70bbb5d42fac38b30
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-02 13:11:10 +0530

    Time: 2 ms (52.38%), Space: 6.4 MB (33.59%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,19 +1,19 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        if (s.length() == 0)
+public:
+    bool isSubsequence(string s, string t) {
+        if (s.size() == 0)
             return true;
-        if (s.length() > t.length())
+        if (s.size() > t.size())
             return false;
         int start = 0, end = 0;
-        
-        while (end < t.length()) {
-            if (t.charAt(end) == s.charAt(start)) {
+        while (end < t.size()) {
+            if (t[end] == s[start]) {
                 start++;
-                if (start == s.length())
+                if (start == s.size())
                     return true;
             }
             end++;
         }
         return false;
     }
-}
+};
