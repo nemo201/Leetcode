fc37c47bcdb3b83c2d008c38d3ea19a9e9b993da
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-03 12:28:27 +0530

    Time: 4 ms (49.49%), Space: 7.4 MB (55.42%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,18 +1,17 @@
 class Solution {
-    public int numberOfArithmeticSlices(int[] nums) {
-        if (nums.length < 3) {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        if (nums.size() < 3)
             return 0;
-        }
+        int sum = 0, cur = 0;
         
-        int cur = 0, sum = 0;
-        for (int i = 2; i < nums.length; i++) {
+        for (int i = 2; i < nums.size(); i++) {
             if (nums[i] - nums[i - 1] == nums[i-1] - nums[i - 2]) {
-                cur += 1;
+                cur++;
                 sum += cur;
-            } else {
+            } else
                 cur = 0;
-            }
         }
         return sum;
     }
-}
+};
