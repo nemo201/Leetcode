6937a6cc126cf1f478233f82871bfd1b360f3404
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-04 13:42:28 +0530

    Time: 16 ms (51.90%), Space: 14.1 MB (49.87%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -1,16 +1,18 @@
 class Solution {
-    public double champagneTower(int poured, int query_row, int query_glass) {
-        double[][] A = new double[102][102];
-        A[0][0] = (double) poured;
-        for (int r = 0; r < query_row; r++) {
-            for (int c = 0; c <= r; c++) {
-                double q = (A[r][c] - 1.0) / 2.0;
-                if (q > 0) {
-                    A[r + 1][c] += q;
-                    A[r + 1][c + 1] += q;
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<double> row = {(double)poured};
+        for (int i = 1; i <= query_row; i++) {
+            vector<double> rowNext(i + 1);
+            for (int j = 0; j < row.size(); j++) {
+                double spill = (row[j] - 1) / 2.0;
+                if (spill > 0) {
+                    rowNext[j] += spill;
+                    rowNext[j + 1] += spill;
                 }
             }
+            row = rowNext;
         }
-        return Math.min(1, A[query_row][query_glass]);
+        return min(1.0, row[query_glass]);
     }
-}
+};
