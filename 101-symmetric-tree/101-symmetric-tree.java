dbcedef90508c441c3068a710064b181e37d4707
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-04 14:26:44 +0530

    Time: 2 ms (12.80%), Space: 42.2 MB (30.05%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -17,16 +17,21 @@ class Solution {
     public boolean isSymmetric(TreeNode root) {
         if (root == null)
             return true;
-        return isSym(root.left, root.right);
-    }
-    
-    public boolean isSym(TreeNode left, TreeNode right) {
-        if (left == null || right == null)
-            return left == right;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root.left);
+        stack.push(root.right);
         
-        if (left.val != right.val)
-            return false;
-        
-        return isSym(left.left, right.right) && isSym(left.right, right.left);
+        while (!stack.isEmpty()) {
+            TreeNode n1 = stack.pop(), n2 = stack.pop();
+            if (n1 == null && n2 == null)
+                continue;
+            if (n1 == null || n2 == null || n1.val != n2.val)
+                return false;
+            stack.push(n1.left);
+            stack.push(n2.right);
+            stack.push(n1.right);
+            stack.push(n2.left);
+        }
+        return true;
     }
 }
