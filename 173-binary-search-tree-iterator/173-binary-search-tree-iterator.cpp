fb6333fae82c11c40daf2e00c347e45d30658c48
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-11 14:30:49 +0530

    Time: 32 ms (76.12%), Space: 24.1 MB (87.00%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -1,44 +1,40 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class BSTIterator {
-    
-    Stack<TreeNode> stack = new Stack<>();
-
-    public BSTIterator(TreeNode root) {
+public:
+    stack<TreeNode*> stk;
+    BSTIterator(TreeNode* root) {
         pushAll(root);
     }
     
-    public int next() {
-        TreeNode node = stack.pop();
-        pushAll(node.right);
-        return node.val;
+    int next() {
+        TreeNode* node = stk.top();
+        stk.pop();
+        pushAll(node->right);
+        return node->val;
     }
     
-    public boolean hasNext() {
-        return !stack.isEmpty();
+    bool hasNext() {
+        return !stk.empty();
     }
     
-    public void pushAll(TreeNode node) {
-        for (; node != null; stack.push(node), node = node.left);
+    void pushAll(TreeNode* node) {
+        for (; node; stk.push(node), node = node->left);
     }
-}
+};
 
 /**
  * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator obj = new BSTIterator(root);
- * int param_1 = obj.next();
- * boolean param_2 = obj.hasNext();
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
  */
