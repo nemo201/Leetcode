3905563241fe8fadd8db0c022c3af3bfafce3070
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-11 13:49:17 +0530

    Time: 131 ms (5.13%), Space: 506.4 MB (16.52%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -1,32 +1,28 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-    public int kthSmallest(TreeNode root, int k) {
-        List<Integer> list = new ArrayList<>();
-        inorder(root, list);
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> vec;
+        inorder(root, vec);
         
-        return list.get(k - 1);
+        return vec[k-1];
     }
     
-    List<Integer> inorder(TreeNode root, List<Integer> list) {
-        if (root == null)
-            return list;
-        
-        inorder(root.left, list);
-        list.add(root.val);
-        return inorder(root.right, list);
+    vector<int> inorder(TreeNode* root, vector<int>& vec) {
+        if (!root)
+            return vec;
+        inorder(root->left, vec);
+        vec.push_back(root->val);
+        return inorder(root->right, vec);
     }
-}
+};
