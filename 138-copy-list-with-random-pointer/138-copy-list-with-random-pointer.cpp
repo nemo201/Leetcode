c351accd445561ac10dc63466cd5c68524843128
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-12 11:11:44 +0530

    Time: 3 ms (99.12%), Space: 11.3 MB (60.48%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -1,76 +1,43 @@
 /*
 // Definition for a Node.
 class Node {
+public:
     int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
     }
-}
+};
 */
 
 class Solution {
-    public Node copyRandomList(Node head) {
-          Node iter = head; 
-          Node front = head;
-
-          // First round: make copy of each node,
-          // and link them together side-by-side in a single list.
-          while (iter != null) {
-            front = iter.next;
-
-            Node copy = new Node(iter.val);
-            iter.next = copy;
-            copy.next = front;
-
-            iter = front;
-          }
-
-          // Second round: assign random pointers for the copy nodes.
-          iter = head;
-          while (iter != null) {
-            if (iter.random != null) {
-              iter.next.random = iter.random.next;
-            }
-            iter = iter.next.next;
-          }
-
-          // Third round: restore the original list, and extract the copy list.
-          iter = head;
-          Node pseudoHead = new Node(0);
-          Node copy = pseudoHead;
-
-          while (iter != null) {
-            front = iter.next.next;
-
-            // extract the copy
-            copy.next = iter.next;
-            copy = copy.next;
-
-            // restore the original list
-            iter.next = front;
-
-            iter = front;
-          }
-
-          return pseudoHead.next;
+public:
+    Node* copyRandomList(Node* head) {
+        Node* newHead, *l1, *l2;
+        if (!head)
+            return NULL;
+        
+        for (l1 = head; l1 != NULL; l1 = l1->next->next) {
+            l2 = new Node(l1->val);
+            l2->next = l1->next;
+            l1->next = l2;
+        }
+        
+        newHead = head->next;
+        for (l1 = head; l1 != NULL; l1 = l1->next->next) {
+            if (l1->random != NULL)
+                l1->next->random = l1->random->next;
+        }
+        for (l1 = head; l1 != NULL; l1 = l1->next) {
+            l2 = l1->next;
+            l1->next = l2->next;
+            if (l2->next != NULL)
+                l2->next = l2->next->next;
+        }
+        return newHead;
     }
-}
-
-// Map<Node, Node> map = new HashMap<>();
-//         Node iter = head;
-//         while (iter != null) {
-//             map.put(iter, new Node(iter.val));
-//             iter = iter.next;
-//         }
-//         Node q = head;
-//         while (q != null) {
-//             map.get(q).next = map.get(q.next);
-//             map.get(q).random = map.get(q.random);
-//             q = q.next;
-//         }
-//         return map.get(head);
+};
