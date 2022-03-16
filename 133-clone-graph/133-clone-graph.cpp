1241abc51c603e21f3ca4ad8266490330684b91f
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-16 15:33:48 +0530

    Time: 12 ms (29.47%), Space: 8.9 MB (27.97%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -21,26 +21,49 @@ public:
 
 class Solution {
 public:
+    unordered_map<Node*, Node*> map;
     Node* cloneGraph(Node* node) {
         if (!node)
             return NULL;
-        Node* copy = new Node(node->val, {});
-        copies[node] = copy;
-        queue<Node*> todo;
-        todo.push(node);
-        while(!todo.empty()) {
-            Node* cur = todo.front();
-            todo.pop();
-            for(Node* neig : cur-> neighbors) {
-                if (copies.find(neig) == copies.end()) {
-                    copies[neig] = new Node(neig -> val, {});
-                    todo.push(neig);
-                }
-                copies[cur]->neighbors.push_back(copies[neig]);
-            }
+        
+        return dfs(node);
+    }
+    
+    Node* dfs(Node* node) {
+        if(map[node])
+            return map[node];
+        Node* copy = new Node(node->val);
+        map[node] = copy;
+        
+        for (Node* nei : node->neighbors) {
+            copy->neighbors.push_back(dfs(nei));
         }
         return copy;
     }
-private:
-    unordered_map<Node*, Node*> copies;
 };
+
+// class Solution {
+// public:
+//     Node* cloneGraph(Node* node) {
+//         if (!node)
+//             return NULL;
+//         Node* copy = new Node(node->val, {});
+//         copies[node] = copy;
+//         queue<Node*> todo;
+//         todo.push(node);
+//         while(!todo.empty()) {
+//             Node* cur = todo.front();
+//             todo.pop();
+//             for(Node* neig : cur-> neighbors) {
+//                 if (copies.find(neig) == copies.end()) {
+//                     copies[neig] = new Node(neig -> val, {});
+//                     todo.push(neig);
+//                 }
+//                 copies[cur]->neighbors.push_back(copies[neig]);
+//             }
+//         }
+//         return copy;
+//     }
+// private:
+//     unordered_map<Node*, Node*> copies;
+// };
