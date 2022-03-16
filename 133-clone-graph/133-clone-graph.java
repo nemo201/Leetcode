f5507b147389b2d1a29e779b315e4d0d6399bbc7
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-16 15:34:08 +0530

    Time: 12 ms (29.47%), Space: 8.7 MB (61.41%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -1,42 +1,69 @@
 /*
 // Definition for a Node.
 class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
         val = 0;
-        neighbors = new ArrayList<Node>();
+        neighbors = vector<Node*>();
     }
-    public Node(int _val) {
+    Node(int _val) {
         val = _val;
-        neighbors = new ArrayList<Node>();
+        neighbors = vector<Node*>();
     }
-    public Node(int _val, ArrayList<Node> _neighbors) {
+    Node(int _val, vector<Node*> _neighbors) {
         val = _val;
         neighbors = _neighbors;
     }
-}
+};
 */
 
 class Solution {
-    Map<Node, Node> oldToNew = new HashMap<>();
-    public Node cloneGraph(Node node) {
-        if (node == null)
-            return node;
+public:
+    unordered_map<Node*, Node*> map;
+    Node* cloneGraph(Node* node) {
+        if (!node)
+            return NULL;
         
         return dfs(node);
     }
     
-    public Node dfs(Node node) {
-            if (oldToNew.containsKey(node)) {
-                return oldToNew.get(node);
-            }
-            Node copy = new Node(node.val);
-            oldToNew.put(node, copy);
-            
-            for (Node nei : node.neighbors) {
-                copy.neighbors.add(dfs(nei));
-            }
-            return copy;
+    Node* dfs(Node* node) {
+        if(map[node])
+            return map[node];
+        Node* copy = new Node(node->val);
+        map[node] = copy;
+        
+        for (Node* nei : node->neighbors) {
+            copy->neighbors.push_back(dfs(nei));
         }
-}
+        return copy;
+    }
+};
+
+// class Solution {
+// public:
+//     Node* cloneGraph(Node* node) {
+//         if (!node)
+//             return NULL;
+//         Node* copy = new Node(node->val, {});
+//         copies[node] = copy;
+//         queue<Node*> todo;
+//         todo.push(node);
+//         while(!todo.empty()) {
+//             Node* cur = todo.front();
+//             todo.pop();
+//             for(Node* neig : cur-> neighbors) {
+//                 if (copies.find(neig) == copies.end()) {
+//                     copies[neig] = new Node(neig -> val, {});
+//                     todo.push(neig);
+//                 }
+//                 copies[cur]->neighbors.push_back(copies[neig]);
+//             }
+//         }
+//         return copy;
+//     }
+// private:
+//     unordered_map<Node*, Node*> copies;
+// };
