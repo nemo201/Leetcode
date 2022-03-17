f1d14cb1ba5ff099a5c9262d5f4e8b94d7e3e4b2
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-17 13:52:55 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (45.05%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int> stk;
+        stk.push(0);
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                stk.push(0);
+            } else {
+                int v = stk.top();
+                stk.pop();
+                int w = stk.top();
+                stk.pop();
+                stk.push(w + max(2 * v, 1));
+            }
+        }
+        return stk.top();
+    }
+};
