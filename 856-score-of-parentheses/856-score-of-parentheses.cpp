c0dcec933ae756cf199c0d5f885f932522cb69d9
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-17 14:01:26 +0530

    Time: 5 ms (14.59%), Space: 6.2 MB (45.05%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        stack<int> stk;
-        stk.push(0);
+        int score = 0, depth = 0;
         
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(') {
-                stk.push(0);
-            } else {
-                int v = stk.top();
-                stk.pop();
-                int w = stk.top();
-                stk.pop();
-                stk.push(w + max(2 * v, 1));
-            }
+            if (s[i] == '(')
+                depth++;
+            else
+                depth--;
+            if (s[i] == ')'&& s[i - 1] == '(')
+                score += pow(2, depth);
         }
-        return stk.top();
+        return score;
     }
 };
+
+// class Solution {
+// public:
+//     int scoreOfParentheses(string s) {
+//         stack<int> stk;
+//         stk.push(0);
+        
+//         for (int i = 0; i < s.size(); i++) {
+//             if (s[i] == '(') {
+//                 stk.push(0);
+//             } else {
+//                 int v = stk.top();
+//                 stk.pop();
+//                 int w = stk.top();
+//                 stk.pop();
+//                 stk.push(w + max(2 * v, 1));
+//             }
+//         }
+//         return stk.top();
+//     }
+// };
