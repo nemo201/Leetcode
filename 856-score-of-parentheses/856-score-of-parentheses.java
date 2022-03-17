9ae8a60dcebdbfb1d617b3ac2ea139ec7b7f3c65
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-17 13:58:26 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (48.47%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,17 +1,32 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(0);
-            } else {
-                int v = stack.pop();
-                int w = stack.pop();
-                stack.push(w + Math.max(2* v, 1));
-            }
+        int score = 0, depth = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                depth++;
+            else
+                depth--;
+            if (s.charAt(i) == ')' && s.charAt(i - 1) == '(')
+                score += Math.pow(2, depth);
         }
-        return stack.pop();
+        return score;
     }
 }
+
+// class Solution {
+//     public int scoreOfParentheses(String s) {
+//         Stack<Integer> stack = new Stack<>();
+//         stack.push(0);
+        
+//         for (char c : s.toCharArray()) {
+//             if (c == '(') {
+//                 stack.push(0);
+//             } else {
+//                 int v = stack.pop();
+//                 int w = stack.pop();
+//                 stack.push(w + Math.max(2* v, 1));
+//             }
+//         }
+//         return stack.pop();
+//     }
+// }
