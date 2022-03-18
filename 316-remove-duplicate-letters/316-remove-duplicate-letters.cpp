27aa1b43ec9b3707bdbdd019d65a186551064507
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-18 15:10:41 +0530

    Time: 7 ms (30.82%), Space: 6.9 MB (20.77%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,33 +1,21 @@
 class Solution {
-    public String removeDuplicateLetters(String sr) {
-
-        int[] res = new int[26]; //will contain number of occurences of character (i+'a')
-        boolean[] visited = new boolean[26]; //will contain if character (i+'a') is present in current result Stack
-        char[] ch = sr.toCharArray();
-        for(char c: ch){  //count number of occurences of character 
-            res[c-'a']++;
+public:
+    string removeDuplicateLetters(string s) {
+    vector<int> cand(256, 0);
+    vector<bool> visited(256, false);
+    for (char c : s)
+        cand[c]++;
+    string result = "0";
+    for (char c : s) {
+        cand[c]--;
+        if (visited[c]) continue;
+        while (c < result.back() && cand[result.back()]) {
+            visited[result.back()] = false;
+            result.pop_back();
         }
-        Stack<Character> st = new Stack<>(); // answer stack
-        int index;
-        for(char s:ch){ 
-            index= s-'a';
-            res[index]--;   //decrement number of characters remaining in the string to be analysed
-            if(visited[index]) //if character is already present in stack, dont bother
-                continue;
-            //if current character is smaller than last character in stack which occurs later in the string again
-            //it can be removed and  added later e.g stack = bc remaining string abc then a can pop b and then c
-            while(!st.isEmpty() && s<st.peek() && res[st.peek()-'a']!=0){ 
-                visited[st.pop()-'a']=false;
-            }
-            st.push(s); //add current character and mark it as visited
-            visited[index]=true;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        //pop character from stack and build answer string from back
-        while(!st.isEmpty()){
-            sb.insert(0,st.pop());
-        }
-        return sb.toString();
+        result += c;
+        visited[c] = true;
     }
+    return result.substr(1);
 }
+};
