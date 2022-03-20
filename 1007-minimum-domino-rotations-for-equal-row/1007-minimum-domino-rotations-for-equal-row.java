9f1cef64581110d0968692387bfd9356db8d34c1
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-20 09:18:35 +0530

    Time: 5 ms (79.08%), Space: 88.6 MB (71.63%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,18 +1,35 @@
 class Solution {
-    public int minDominoRotations(int[] tops, int[] bottoms) {
-        int[] cA = new int[7], cB = new int[7], same = new int[7];
-        
-        int n = tops.length;
-        for (int i = 0; i < n ; i++) {
-            cA[tops[i]]++;
-            cB[bottoms[i]]++;
-            if (tops[i] == bottoms[i])
-                same[tops[i]]++;
+    public int minDominoRotations(int[] A, int[] B) {
+        int n = A.length;
+        for (int i = 0, a = 0, b = 0; i < n && (A[i] == A[0] || B[i] == A[0]); ++i) {
+            if (A[i] != A[0]) a++;
+            if (B[i] != A[0]) b++;
+            if (i == n - 1) return Math.min(a, b);
         }
-        for (int i = 1; i < 7; i++) {
-            if (cA[i] + cB[i] - same[i] == n)
-                return n - Math.max(cA[i], cB[i]);
+        for (int i = 0, a = 0, b = 0; i < n && (A[i] == B[0] || B[i] == B[0]); ++i) {
+            if (A[i] != B[0]) a++;
+            if (B[i] != B[0]) b++;
+            if (i == n - 1) return Math.min(a, b);
         }
         return -1;
     }
 }
+
+// class Solution {
+//     public int minDominoRotations(int[] tops, int[] bottoms) {
+//         int[] cA = new int[7], cB = new int[7], same = new int[7];
+        
+//         int n = tops.length;
+//         for (int i = 0; i < n ; i++) {
+//             cA[tops[i]]++;
+//             cB[bottoms[i]]++;
+//             if (tops[i] == bottoms[i])
+//                 same[tops[i]]++;
+//         }
+//         for (int i = 1; i < 7; i++) {
+//             if (cA[i] + cB[i] - same[i] == n)
+//                 return n - Math.max(cA[i], cB[i]);
+//         }
+//         return -1;
+//     }
+// }
