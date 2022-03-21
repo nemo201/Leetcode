13a968981d111f884d32159054a7e93041e4b519
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-21 13:19:56 +0530

    Time: 6 ms (46.00%), Space: 6.5 MB (96.62%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int last[26];
+        for (int i = 0; i < s.size(); i++) {
+            last[s[i] - 'a'] = i;
+        }
+        
+        int j = 0, anchor = 0;
+        vector<int> ans;
+        for (int i = 0; i < s.size(); i++) {
+            j = max(j, last[s[i] - 'a']);
+            if (i == j) {
+                ans.push_back(i - anchor + 1);
+                anchor = i + 1;
+            }
+        }
+        return ans;
+    }
+};
