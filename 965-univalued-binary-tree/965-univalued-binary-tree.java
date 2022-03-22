a54e67a3b67000849e8ec842cbf89aace93ce1da
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-22 15:16:19 +0530

    Time: 1 ms (18.78%), Space: 42.1 MB (17.32%) - LeetHub

--- 965-univalued-binary-tree/965-univalued-binary-tree.java
+++ 965-univalued-binary-tree/965-univalued-binary-tree.java
@@ -14,18 +14,39 @@
  * }
  */
 class Solution {
+    List<Integer> list;
     public boolean isUnivalTree(TreeNode root) {
-        if (root == null)
-            return true;
-        
-        if(root.left != null)
-        if(root.val != root.left.val)
-            return false;
-    
-        if(root.right != null)
-            if(root.val != root.right.val)
+        list = new ArrayList();
+        dfs(root);
+        for (int v : list) {
+            if (v != list.get(0))
                 return false;
-
-        return isUnivalTree(root.left) && isUnivalTree(root.right);
+        }
+        return true;
+    }
+    
+    public void dfs(TreeNode root) {
+        if (root != null) {
+            list.add(root.val);
+            dfs(root.left);
+            dfs(root.right);
+        }
     }
 }
+
+// class Solution {
+//     public boolean isUnivalTree(TreeNode root) {
+//         if (root == null)
+//             return true;
+        
+//         if(root.left != null)
+//         if(root.val != root.left.val)
+//             return false;
+    
+//         if(root.right != null)
+//             if(root.val != root.right.val)
+//                 return false;
+
+//         return isUnivalTree(root.left) && isUnivalTree(root.right);
+//     }
+// }
