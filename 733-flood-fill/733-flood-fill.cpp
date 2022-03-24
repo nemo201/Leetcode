16e20fc95c35ecc395127b6b5fdf65c6c5ec33d7
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-24 13:52:45 +0530

    Time: 15 ms (43.69%), Space: 14 MB (80.61%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int col = image[sr][sc];
-        if (col != newColor)
-            dfs(image, sr, sc, col, newColor);
+        int color = image[sr][sc];
+        if (color != newColor)
+            dfs(image, sr, sc, color, newColor);
+        
         return image;
     }
     
-    void dfs(vector<vector<int>>& image, int r, int c, int col, int newColor) {
-        if (image[r][c] == col) {
+    void dfs(vector<vector<int>>& image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
             image[r][c] = newColor;
             if (r >= 1)
-                dfs(image, r - 1, c, col, newColor);
+                dfs(image, r - 1, c, color, newColor);
             if (c >= 1)
-                dfs(image, r, c - 1, col, newColor);
-            if (r+1 < image.size())
-                dfs(image, r + 1, c, col, newColor);
-            if (c+1 < image[0].size())
-                dfs(image, r, c + 1, col, newColor);
+                dfs(image, r, c - 1, color, newColor);
+            if (r + 1 < image.size())
+                dfs(image, r + 1, c, color, newColor);
+            if (c + 1 < image[0].size())
+                dfs(image, r, c + 1, color, newColor);
         }
     }
 };
