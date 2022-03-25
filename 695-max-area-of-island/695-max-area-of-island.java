d27912b9de5c3fc6b155fce0a4d334d49a160ed3
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-25 13:53:36 +0530

    Time: 5 ms (33.42%), Space: 47.2 MB (58.09%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -0,0 +1,24 @@
+class Solution {
+    int[][] grid;
+    boolean[][] visited;
+    
+    public int maxAreaOfIsland(int[][] grid) {
+        this.grid = grid;
+        visited = new boolean[grid.length][grid[0].length];
+        int ans = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                ans = Math.max(ans, area(r,c));
+            }
+        }
+        return ans;
+    }
+    
+    public int area(int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || visited[r][c] || grid[r][c] == 0)
+            return 0;
+        visited[r][c] = true;
+        
+        return (1 + area(r+1, c) + area(r-1, c) + area(r, c-1) + area(r, c+1));
+    }
+}
