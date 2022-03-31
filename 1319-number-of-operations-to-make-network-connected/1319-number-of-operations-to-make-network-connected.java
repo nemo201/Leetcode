e78fb9d599c5b52a87c456471190e0ac30931c00
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-03-31 15:50:03 +0530

    Time: 12 ms (56.37%), Space: 57.4 MB (98.40%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        if (connections.length < n - 1) return -1; // To connect all nodes need at least n-1 edges
+        List<Integer>[] graph = new List[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int[] c : connections) {
+            graph[c[0]].add(c[1]);
+            graph[c[1]].add(c[0]);
+        }
+        int components = 0;
+        boolean[] visited = new boolean[n];
+        for (int v = 0; v < n; v++) components += bfs(v, graph, visited);
+        return components - 1; // Need (components-1) cables to connect components together
+    }
+    int bfs(int src, List<Integer>[] graph, boolean[] visited) {
+        if (visited[src]) return 0;
+        visited[src] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(src);
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            for (int v : graph[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.offer(v);
+                }
+            }
+        }
+        return 1;
+    }
+}
