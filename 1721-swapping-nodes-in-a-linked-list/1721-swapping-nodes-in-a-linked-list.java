de5b6ec344b0cfe2fa60c0239b4eaca285811aa2
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-04-04 14:14:58 +0530

    Time: 3 ms (81.58%), Space: 183.1 MB (28.28%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -1,25 +1,27 @@
 /**
  * Definition for singly-linked list.
- * struct ListNode {
+ * public class ListNode {
  *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
 class Solution {
-public:
-    ListNode* swapNodes(ListNode* head, int k) {
-    ListNode *n1 = nullptr, *n2 = nullptr;
-    for (auto p = head; p != nullptr; p = p->next) {
-        n2 = n2 == nullptr ? nullptr : n2->next;
-        if (--k == 0) {
-            n1 = p;
-            n2 = head;
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode n1 = null, n2 = null;
+        
+        for (var p = head; p != null; p = p.next) {
+            n2 = n2 == null ? null : n2.next;
+                if (--k == 0) {
+                    n1 = p;
+                    n2 = head;
+            }
         }
+        var tmp = n1.val;
+        n1.val = n2.val;
+        n2.val = tmp;
+        return head;
     }
-    swap(n1->val, n2->val);
-    return head;
 }
-};
