2a286c342b27ac976652fbcd84d2f8d3b505da00
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-04-07 15:37:22 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (78.96%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,22 +1,16 @@
 class Solution {
-    public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        for (int v : stones)
-            maxHeap.offer(v);
-    
-        int x;
-        int y;
-        while(maxHeap.size() > 1) {
-            y = maxHeap.poll();
-            x = maxHeap.poll();
-
-            if (y > x)
-                maxHeap.offer(y - x);
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(), stones.end());
+        while(pq.size() > 1) {
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+            
+            if (x != y)
+                pq.push(y - x);
         }
-
-        if (maxHeap.size() == 0)
-            return 0;
-
-        return maxHeap.poll();
+        return pq.empty() ? 0 : pq.top();
     }
-}
+};
