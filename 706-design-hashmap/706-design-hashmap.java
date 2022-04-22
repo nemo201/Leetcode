f1d30664a987561d1f118593ae65c3202fcba5ce
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-04-22 12:42:35 +0530

    Time: 34 ms (50.61%), Space: 62.2 MB (50.34%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,22 +1,65 @@
 class MyHashMap {
-    Map<Integer, Integer> map;
+    LinkedList<Node>[] map;
+    public static int size = 769;
 
     public MyHashMap() {
-        map = new HashMap();
+        map = new LinkedList[size];
     }
     
     public void put(int key, int value) {
-        map.put(key, value);
+        int bucket = key % size;
+        if (map[bucket] == null) {
+            map[bucket] = new LinkedList<Node>();
+            map[bucket].add(new Node(key, value));
+        } else {
+            for (Node node : map[bucket]) {
+                if (node.key == key) {
+                    node.val = value;
+                    return;
+                }
+            }
+            map[bucket].add(new Node(key, value));
+        }
     }
     
     public int get(int key) {
-        if (map.containsKey(key))
-            return map.get(key);
+        int bucket = key % size;
+        LinkedList<Node> nodes = map[bucket];
+        if (nodes == null)
+            return -1;
+        
+        for (Node node : nodes)
+            if (node.key == key)
+                return node.val;
+        
         return -1;
     }
     
     public void remove(int key) {
-        map.remove(key);
+        int bucket = key % size;
+        Node toRemove = null;
+        if (map[bucket] == null)
+            return;
+        else {
+            for (Node node : map[bucket]) {
+                if (node.key == key)
+                    toRemove = node;
+            }
+            if (toRemove == null)
+                return;
+            
+            map[bucket].remove(toRemove);
+        }
+    }
+}
+
+class Node {
+    public int key;
+    public int val;
+    
+    public Node(int key, int val) {
+        this.key = key;
+        this.val = val;
     }
 }
 
