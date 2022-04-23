6860bd47bcfac6531f7c1f6f40c9b2327ed0051d
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-04-23 11:55:42 +0530

    Time: 5 ms (21.53%), Space: 8.8 MB (62.59%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,15 +1,11 @@
 class Solution {
-    public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        for (int i = n -1; i >= 0; i--) {
-            if (digits[i] < 9){
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
-        }
-        int[] newNumber = new int[n + 1];
-        newNumber[0] = 1;
-        return newNumber;
+public:
+    vector<int> plusOne(vector<int>& digits) {
+    for (int i=digits.size(); i--; digits[i] = 0)
+        if (digits[i]++ < 9)
+            return digits;
+    digits[0]++;
+    digits.push_back(0);
+    return digits;
     }
-}
+};
