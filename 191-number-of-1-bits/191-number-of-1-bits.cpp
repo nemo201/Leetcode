87cbc1136ff483de38e2383754141d0fce6a1db2
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-05-26 12:10:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (79.17%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ones = 0;
+        while (n != 0) {
+            ones += n & 1;
+            n = n >> 1;
+        }
+        return ones;
+    }
+};
