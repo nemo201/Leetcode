38d5ef939f7ea1c437ca7c4bb7b526383b2669b4
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-17 17:48:45 +0530

    Time: 61 ms (30.89%), Space: 117.6 MB (45.53%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,11 +1,31 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (a, b) -> sd(a) - sd(b));
+        // Use a lambda comparator to sort the PQ by farthest distance
+        Queue<int[]> maxPQ = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        for (int i = 0; i < points.length; i++) {
+            int[] entry = {squaredDistance(points[i]), i};
+            if (maxPQ.size() < k) {
+                // Fill the max PQ up to k points
+                maxPQ.add(entry);
+            } else if (entry[0] < maxPQ.peek()[0]) {
+                // If the max PQ is full and a closer point is found,
+                // discard the farthest point and add this one
+                maxPQ.poll();
+                maxPQ.add(entry);
+            }
+        }
         
-        return Arrays.copyOf(points, k);
+        // Return all points stored in the max PQ
+        int[][] answer = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            int entryIndex = maxPQ.poll()[1];
+            answer[i] = points[entryIndex];
+        }
+        return answer;
     }
     
-    private int sd(int[] points) {
-        return points[0] * points[0] + points[1] * points[1];
+    private int squaredDistance(int[] point) {
+        // Calculate and return the squared Euclidean distance
+        return point[0] * point[0] + point[1] * point[1];
     }
-}
+};
