e618898b224bd97a077393a95b666dfc4f7a6782
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-18 16:12:35 +0530

    Time: 4 ms (87.11%), Space: 43.6 MB (66.91%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        findComb(0, candidates, target, list, new ArrayList());
+        return list;
+    }
+    
+    public void findComb(int index, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+        if (target == 0){
+            ans.add(new ArrayList(ds));
+            return;
+        }
+        
+        for (int i = index; i < arr.length; i++) {
+            if (i > index && arr[i] == arr[i - 1])
+                continue;
+            if (arr[i] > target)
+                break;
+            
+            ds.add(arr[i]);
+            findComb(i + 1, arr, target - arr[i], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+    }
+}
