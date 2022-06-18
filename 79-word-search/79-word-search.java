e508b1f024256db8b6cbce8c2701d43bbb043861
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-18 17:24:35 +0530

    Time: 128 ms (76.92%), Space: 42.8 MB (20.51%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
         int rows = board.length, cols = board[0].length;
-        
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 if (dfs(r, c, 0, rows, cols, board, word))
@@ -14,17 +13,18 @@ class Solution {
     public boolean dfs(int r, int c, int i, int rows, int cols, char[][] board, String word) {
         if (i == word.length())
             return true;
-        
         if (r < 0 || r >= rows || c < 0 || c >= cols || word.charAt(i) != board[r][c] || board[r][c] == '#')
             return false;
         
         char ch = board[r][c];
         board[r][c] = '#';
         boolean result;
+        
         result = dfs(r + 1, c, i + 1, rows, cols, board, word) || dfs(r - 1, c, i + 1, rows, cols, board, word) ||
                  dfs(r, c + 1, i + 1, rows, cols, board, word) || dfs(r, c - 1, i + 1, rows, cols, board, word);
         
         board[r][c] = ch;
+        
         return result;
     }
 }
