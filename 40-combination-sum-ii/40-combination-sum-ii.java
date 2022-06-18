c12e9356a585041f442048c3248356128107b3bc
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-18 21:21:06 +0530

    Time: 3 ms (94.88%), Space: 44.8 MB (12.48%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,3 +1,29 @@
+// class Solution {
+//     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+//         List<List<Integer>> list = new ArrayList<>();
+//         Arrays.sort(candidates);
+//         findComb(0, candidates, target, list, new ArrayList());
+//         return list;
+//     }
+    
+//     public void findComb(int index, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+//         if (target == 0){
+//             ans.add(new ArrayList(ds));
+//             return;
+//         }
+        
+//         for (int i = index; i < arr.length; i++) {
+//             if (i > index && arr[i] == arr[i - 1])
+//                 continue;
+//             if (arr[i] > target)
+//                 break;
+            
+//             ds.add(arr[i]);
+//             findComb(i + 1, arr, target - arr[i], ans, ds);
+//             ds.remove(ds.size() - 1);
+//         }
+//     }
+// }
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> list = new ArrayList<>();
@@ -7,7 +33,7 @@ class Solution {
     }
     
     public void findComb(int index, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
-        if (target == 0){
+        if (target == 0) {
             ans.add(new ArrayList(ds));
             return;
         }
