03014a973f6dd825ef016ee50131e8edd4b8f299
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-18 15:48:51 +0530

    Time: 3 ms (93.85%), Space: 45.3 MB (37.14%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -5,18 +5,18 @@ class Solution {
         return list;
     }
     
-    public void findComb(int index, int[] arr, int target, List<List<Integer>> list, List<Integer> ds) {
+    public void findComb(int index, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
         if (index == arr.length) {
             if (target == 0)
-                list.add(new ArrayList<>(ds));
+                ans.add(new ArrayList<>(ds));
             return;
         }
         
         if (arr[index] <= target) {
             ds.add(arr[index]);
-            findComb(index, arr, target - arr[index], list, ds);
+            findComb(index, arr, target - arr[index], ans, ds);
             ds.remove(ds.size() - 1);
         }
-        findComb(index + 1, arr, target, list, ds);
+        findComb(index + 1, arr, target, ans, ds);
     }
 }
