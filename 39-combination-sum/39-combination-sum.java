a7cecdf5d3986f46e752731e590596dc3865c000
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-18 15:43:38 +0530

    Time: 4 ms (83.05%), Space: 45 MB (51.66%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,23 +1,22 @@
 class Solution {
-    public void findcomb(int index, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        findComb(0, candidates, target, list, new ArrayList());
+        return list;
+    }
+    
+    public void findComb(int index, int[] arr, int target, List<List<Integer>> list, List<Integer> ds) {
         if (index == arr.length) {
-            if (target == 0) {
-                ans.add(new ArrayList<>(ds));
-            }
+            if (target == 0)
+                list.add(new ArrayList<>(ds));
             return;
         }
         
         if (arr[index] <= target) {
             ds.add(arr[index]);
-            findcomb(index, arr, target - arr[index], ans, ds);
+            findComb(index, arr, target - arr[index], list, ds);
             ds.remove(ds.size() - 1);
         }
-        findcomb(index + 1, arr, target, ans, ds);
-    }
-    
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        findcomb(0, candidates, target, ans, new ArrayList());
-        return ans;
+        findComb(index + 1, arr, target, list, ds);
     }
 }
