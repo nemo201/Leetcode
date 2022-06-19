7697bd8b3a1083727fccd0e17a647ed9043397a4
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-19 22:55:38 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (46.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -2,27 +2,26 @@ class Solution {
     static String[] keypad = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     
     public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        List<String> res = new ArrayList();
         
         if (digits.length() == 0)
-            return res;
+            return list;
         
-        dfs(sb, 0, digits, keypad, res);
-        return res;
+        dfs(sb, 0, digits, keypad, list);
+        return list;
     }
     
-    private void dfs(StringBuilder sb, int index, String digits, String[] keypads, List<String> res) {
+    public void dfs(StringBuilder sb, int index, String digits, String[] keypad, List<String> ans) {
         if (index == digits.length()) {
-            res.add(sb.toString());
+            ans.add(sb.toString());
             return;
         }
         
-        String options = keypad[digits.charAt(index) - '0'];
-        System.out.println(options);
-        for (char opt : options.toCharArray()) {
+        String option = keypad[digits.charAt(index) - '0'];
+        for (char opt : option.toCharArray()) {
             sb.append(opt);
-            dfs(sb, index + 1, digits, keypad, res);
+            dfs(sb, index + 1, digits, keypad, ans);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
