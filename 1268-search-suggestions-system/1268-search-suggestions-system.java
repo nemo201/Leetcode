5c41f4caf0d2f5cafa228e2163d828ee6d79e53d
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-19 09:45:21 +0530

    Time: 33 ms (72.67%), Space: 60.8 MB (57.96%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Arrays.sort(products);
+        List<List<String>> result = new ArrayList();
+        int start = 0, bsStart = 0, n = products.length;
+        String prefix = new String();
+        
+        for (char c : searchWord.toCharArray()) {
+            prefix += c;
+            
+            start = lower_bound(products, bsStart, prefix);
+            result.add(new ArrayList<>());
+            
+            for (int i = start; i < Math.min(start + 3, n); i++) {
+                if (products[i].length() < prefix.length() || !products[i].substring(0, prefix.length()).equals(prefix))
+                    break;
+                result.get(result.size() - 1).add(products[i]);
+            }
+            bsStart = Math.abs(start);
+        }
+        return result;
+    }
+    
+    int lower_bound(String[] arr, int start, String word) {
+        int i = start, j = arr.length, mid;
+        while (i < j) {
+            mid = (i + j) / 2;
+            if (arr[mid].compareTo(word) >= 0)
+                j = mid;
+            else
+                i =  mid + 1;
+        }
+        return i;
+    }
+}
