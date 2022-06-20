7b603cd386602fe9a8cea297e077bda4e9c41c1d
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-20 23:28:31 +0530

    Time: 3 ms (53.68%), Space: 51.6 MB (56.17%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -11,11 +11,6 @@ class Solution {
             for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == 'O')
                     board[i][j] = 'X';
-            }
-        }
-        
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == 'T')
                     board[i][j] = 'O';
             }
