0828b1491eb12f849ffba17445729d0bc31e2ca5
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-20 17:44:47 +0530

    Time: 8 ms (64.32%), Space: 55.3 MB (34.18%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,65 +1,36 @@
 class Solution {
-
-int rows = 0;
-int cols = 0;
-
-int [][] heightsGlobal = null;
-
-public List<List<Integer>> pacificAtlantic(int[][] heights) {
-    heightsGlobal = heights;
-    rows = heights.length;
-    cols = heights[0].length;
-
-    Set<String> po = new HashSet<>();
-    Set<String> alt = new HashSet<>();
-            
-    for(int i = 0; i < cols; i++){
-        //po cols
-        dfs(0,i, po, heights[0][i]);
-        //alt cols
-        dfs(rows-1,i, alt, heights[rows-1][i]);
-    }
-    
-    for(int j = 0; j<rows; j++){
-        //po rows
-        dfs(j,0, po, heights[j][0]);
-        //alt cols
-        dfs(j,cols-1, alt, heights[j][cols-1]);
-    }
-    
-    List<List<Integer>> result = new ArrayList<>();
-    
-    for(int k = 0; k<rows; k++){
-        for(int l = 0; l<cols; l++){
-           String cell = Integer.toString(k) + ":" + Integer.toString(l);
-            
-            if(po.contains(cell) && alt.contains(cell)){
-                String [] res = cell.split(":");
-                
-                List<Integer> resList = new ArrayList<>();
-                resList.add(Integer.valueOf(res[0]));
-                resList.add(Integer.valueOf(res[1]));
-                result.add(resList);
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+        int[] dy= new int[]{1, -1, 0, 0};
+        int[] dx= new int[]{0, 0, 1, -1};
+        for (int i = 0; i < m; i++) {
+            explore(pacific, heights, i, 0, m, n, dx, dy);
+            explore(atlantic, heights, i, n - 1, m, n, dx, dy);
+        }
+        
+        for (int j = 0; j < n; j++) {
+            explore(pacific, heights, 0, j, m, n, dx, dy);
+            explore(atlantic, heights, m - 1, j, m, n, dx, dy);
+        }
+        List<List<Integer>> solution = new ArrayList<List<Integer>>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j])
+                    solution.add(new ArrayList<Integer>(Arrays.asList(i, j)));
             }
         }
+        return solution;
     }
     
-    return result;
-    
-}
-
-public void dfs(int r, int c, Set<String> visited, int prevHeight) {
-    String cell = Integer.toString(r) + ":" + Integer.toString(c);
-    System.out.println(cell);
-    if(visited.contains(cell) || r < 0 || r == rows || c < 0 || c == cols || heightsGlobal[r][c] < prevHeight){
-        return;
+    public void explore(boolean[][] ocean, int[][] heights, int r, int c, int m, int n, int[] dx, int[] dy) {
+        ocean[r][c] = true;
+        for (int i = 0; i < 4; i++) {
+            if (r + dx[i] >= 0 && r + dx[i] < m && c + dy[i] >= 0 && c + dy[i] < n
+               && !ocean[r + dx[i]][c + dy[i]] && heights[r + dx[i]][c + dy[i]] >= heights[r][c] ) 
+                explore(ocean, heights, r + dx[i], c + dy[i], m, n, dx, dy);
+        }
     }
-    
-    visited.add(Integer.toString(r) + ":" + Integer.toString(c));
-    
-    dfs(r+1,c,visited, heightsGlobal[r][c]);
-    dfs(r-1,c,visited, heightsGlobal[r][c]);
-    dfs(r,c+1,visited, heightsGlobal[r][c]);
-    dfs(r,c-1,visited, heightsGlobal[r][c]);
-}
 }
