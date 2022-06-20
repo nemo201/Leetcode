e3bc2428038301b395a85a3d602bd3e16342407c
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-20 16:50:39 +0530

    Time: 66 ms (5.66%), Space: 43 MB (69.02%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -20,23 +20,24 @@ class Node {
 
 class Solution {
     Map<Node, Node> oldToNew = new HashMap<>();
+    
     public Node cloneGraph(Node node) {
         if (node == null)
             return node;
         
-        return dfs(node);
+        return dfs (node);
     }
     
     public Node dfs(Node node) {
-            if (oldToNew.containsKey(node)) {
-                return oldToNew.get(node);
-            }
-            Node copy = new Node(node.val);
-            oldToNew.put(node, copy);
-            
-            for (Node nei : node.neighbors) {
-                copy.neighbors.add(dfs(nei));
-            }
-            return copy;
-        }
+        if (oldToNew.containsKey(node))
+            return oldToNew.get(node);
+        
+        Node copy = new Node(node.val);
+        oldToNew.put(node, copy);
+        
+        for (Node nei : node.neighbors)
+            copy.neighbors.add(dfs(nei));
+        
+        return copy;
+    }
 }
