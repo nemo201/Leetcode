de161acc7c5032ca67ef8b81d2102d21c0512150
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-22 17:21:52 +0530

    Time: 219 ms (35.06%), Space: 118.4 MB (6.85%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -15,13 +15,16 @@ class Solution {
         while(!queue.isEmpty()){
             int size = queue.size();
             for(int i = 0; i < size; i++){
+                
                 String word = queue.poll();
-                if(word.equals(endWord)) return changes;
+                
+                if(word.equals(endWord)) 
+                    return changes;
                 
                 for(int j = 0; j < word.length(); j++){
-                    for(int k = 'a'; k <= 'z'; k++){
+                    for(char c = 'a'; c <= 'z'; c++){
                         char arr[] = word.toCharArray();
-                        arr[j] = (char) k;
+                        arr[j] = c;
                         
                         String str = new String(arr);
                         if(set.contains(str) && !visited.contains(str)){
