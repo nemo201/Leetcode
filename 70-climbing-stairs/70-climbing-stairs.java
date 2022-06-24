1b4216fbbfb2ae6eb253b6742aec90cb310a3ff8
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-24 14:21:44 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (63.54%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -3,7 +3,7 @@ class Solution {
         int one = 1, two = 1;
         for (int i = 0; i < n - 1; i++) {
             int temp = one;
-            one = one + two;
+            one += two;
             two = temp;
         }
         return one;
