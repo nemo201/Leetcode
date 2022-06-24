2aece77b69eca1eae50d56af6acabb2e5199a01e
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-24 16:48:18 +0530

    Time: 5 ms (23.85%), Space: 7.6 MB (97.33%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1)
+            return nums[0];
+        int f1= helper(0, nums.size() -1, nums);
+        int f2 = helper(1, nums.size(), nums);
+        
+        return max(f1, f2);
+    }
+    
+    int helper(int i, int j, vector<int>& nums) {
+        int rob1 = 0, rob2 = 0;
+        
+        for (; i < j; i++) {
+            int temp = max(nums[i] + rob1, rob2);
+            rob1 = rob2; rob2 = temp;
+        }
+        return rob2;
+    }
+};
