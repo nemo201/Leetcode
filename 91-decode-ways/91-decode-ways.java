63978b5f0500b9a2d03e037713e9d52973dd3093
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-25 15:45:54 +0530

    Time: 1 ms (98.20%), Space: 41.6 MB (81.21%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,18 +1,32 @@
 class Solution {
-    public int numDecodings(String s) {
-        int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[n] = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            if (s.charAt(i) != '0') {
-                dp[i] = dp[i + 1];
-                if (i < n - 1 && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7'))
-                    dp[i] += dp[i + 2];
-            }
+    	public int numDecodings(String s) {
+        int dp1=1, dp2=0, n=s.length();
+        for(int i=n-1;i>=0;i--) {
+            int dp=s.charAt(i)=='0'?0:dp1;
+            if(i<n-1&&(s.charAt(i)=='1'||s.charAt(i)=='2'&&s.charAt(i+1)<'7'))
+                dp+=dp2;
+            dp2=dp1;
+            dp1=dp;
         }
-        return dp[0];
+        return dp1;
     }
 }
+
+// class Solution {
+//     public int numDecodings(String s) {
+//         int n = s.length();
+//         int[] dp = new int[n + 1];
+//         dp[n] = 1;
+//         for (int i = n - 1; i >= 0; i--) {
+//             if (s.charAt(i) != '0') {
+//                 dp[i] = dp[i + 1];
+//                 if (i < n - 1 && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7'))
+//                     dp[i] += dp[i + 2];
+//             }
+//         }
+//         return dp[0];
+//     }
+// }
 
 
 //memorization
