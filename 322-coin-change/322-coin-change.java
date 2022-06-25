f281cab783dec20abce65752ae83ad9ca8b74251
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-25 17:11:21 +0530

    Time: 30 ms (48.20%), Space: 45.1 MB (51.94%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,19 +1,18 @@
-public class Solution {
-public int coinChange(int[] coins, int amount) {
-    if(amount<1) return 0;
-    int[] dp = new int[amount+1];
-    int sum = 0;
-    
-	while(++sum<=amount) {
-		int min = -1;
-    	for(int coin : coins) {
-    		if(sum >= coin && dp[sum-coin]!=-1) {
-    			int temp = dp[sum-coin]+1;
-    			min = min<0 ? temp : (temp < min ? temp : min);
-    		}
-    	}
-    	dp[sum] = min;
-	}
-	return dp[amount];
-}
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount < 1)
+            return 0;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+        }
+        return dp[amount] != amount + 1 ? dp[amount] : -1;
+    }
 }
