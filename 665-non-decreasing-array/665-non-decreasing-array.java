b841fd1c294ed468fe648474e2606720b0f2a771
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-25 08:33:27 +0530

    Time: 1 ms (97.53%), Space: 53.8 MB (59.83%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean checkPossibility(int[] nums) {
+        
+        int cnt = 0;    
+        int len = nums.length;
+        int break_index = -1;
+        
+        for(int i=0;i<len-1 && cnt < 2;++i){           
+            if(nums[i] > nums[i+1]){
+                cnt += 1;
+                break_index = i+1;
+            }            
+        }
+        
+        if(cnt > 1) return false;     
+        if(break_index == len-1 || break_index == 1) return true;
+        if(cnt == 1 && nums[break_index-1] <= nums[break_index+1]) return true;
+        if(cnt == 1 && nums[break_index-2] <= nums[break_index]) return true;
+        
+        return cnt == 0;
+    }
+}
