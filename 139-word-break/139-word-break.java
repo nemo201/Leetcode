ab3582c2fb0f2addeb2937f0ef2c7d976020073f
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-26 13:00:23 +0530

    Time: 14 ms (36.39%), Space: 47.4 MB (23.34%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordList) {
         boolean[] T = new boolean[s.length() + 1];
-        Set<String> set = new HashSet<>();
-        for (String word : wordList) {
-            set.add(word);
-        }
+        Set<String> set = new HashSet<>(wordList);
+        
         T[0] = true;
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
