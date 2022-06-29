9be9a1fe6070019752025c8b7206b8f5a89777b8
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-29 15:27:15 +0530

    Time: 2 ms (91.57%), Space: 67.1 MB (76.68%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,3 +1,4 @@
+//greedy
 class Solution {
     public boolean canJump(int[] nums) {
         int last = nums.length - 1;
@@ -9,3 +10,22 @@ class Solution {
         return last <= 0;
     }
 }
+
+// dp
+// static int[] dp;
+//     public boolean canJump(int[] nums) {
+//         dp = new int[nums.length];
+//         Arrays.fill(dp,-1);
+//         return helper(0, nums) == 1;
+//     }
+//     private static int helper(int i , int[] nums){
+//         if(i == nums.length-1){
+//                 return 1;
+//         }
+//         if(dp[i] != -1) return dp[i];
+//         for(int k = 1; k <= nums[i];k++){
+//             if(helper(i+k,nums) == 1) return dp[i] = 1;
+//         }
+//         return dp[i] = 0 ;
+//     }
+// }
