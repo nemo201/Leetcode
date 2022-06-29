1be346bbc634953c9d97578cb1b3da99ced79a7c
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-06-29 15:15:20 +0530

    Time: 2 ms (91.57%), Space: 68.2 MB (36.49%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int last = nums.length - 1, i;
+        
+        for (i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= last)
+                last = i;
+        }
+        return last <= 0;
+    }
+}
