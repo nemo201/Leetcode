a94f65e8cd6abd3f03b1edea4b8af622585c3f51
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-01 00:05:54 +0530

    Time: 3 ms (97.66%), Space: 40.5 MB (99.24%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -6,14 +6,14 @@ class Solution {
         }
         
         int j = 0, anchor = 0;
-        List<Integer> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
             j = Math.max(j, last[s.charAt(i) - 'a']);
-            if (i == j){
-                ans.add(i - anchor + 1);
+            if (i == j) {
+                list.add(i - anchor + 1);
                 anchor = i + 1;
             }
         }
-        return ans;
+        return list;
     }
 }
