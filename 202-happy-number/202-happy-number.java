211d6d2f79bc350c8961e75d08a6de3fe9fa9761
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-01 21:58:30 +0530

    Time: 3 ms (44.14%), Space: 41.7 MB (26.84%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    private int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            sum += d * d;
+        }
+        return sum;
+    }
+    
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+}
