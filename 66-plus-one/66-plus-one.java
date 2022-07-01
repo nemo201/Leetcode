be2cb2e5809864d1f65c8639b1213b342a727291
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-01 22:48:42 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (7.37%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        for (int i = n -1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9){
                 digits[i]++;
                 return digits;
@@ -10,6 +10,7 @@ class Solution {
         }
         int[] newNumber = new int[n + 1];
         newNumber[0] = 1;
+        
         return newNumber;
     }
 }
