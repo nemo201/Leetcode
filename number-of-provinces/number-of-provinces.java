da0b0d0494b8dd2f9eef5339faf0d1250afaf5b5
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-04 17:46:21 +0530

    Time: 3 ms, Memory: 54.2 MB - LeetHub

--- number-of-provinces/number-of-provinces.java
+++ number-of-provinces/number-of-provinces.java
@@ -1,22 +1,61 @@
 class Solution {
-    public void dfs(int[][]M, int[] visited, int i) {
-        for (int j = 0; j < M.length; j++) {
-            if(M[i][j] == 1 && visited[j] == 0) {
-                visited[j] = 1;
-                dfs(M, visited, j);
+    class uf {
+        private int[] root;
+        private int[] rank;
+        private int count;
+        
+        public uf(int size) {
+            root = new int[size];
+            rank = new int[size];
+            count = size;
+            
+            for (int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
             }
+        }
+        
+        public int find(int x) {
+            if (x == root[x])
+                return x;
+            return root[x] = find(root[x]);
+        }
+        
+        public void union(int x, int y) {
+            int rx = find(x);
+            int ry = find(y);
+            
+            if (rx != ry) {
+                if (rank[rx] > rank[ry])
+                    root[ry] = rx;
+                else if (rank[rx] < rank[ry])
+                    root[rx] = ry;
+                else {
+                    root[ry] = rx;
+                    rank[rx]++;
+                }
+                count--;
+            }
+        }
+        
+        public int getCount() {
+            return count;
         }
     }
     
-    public int findCircleNum(int[][] M) {
-        int[] visited= new int[M.length];
-        int count = 0;
-        for (int i = 0; i < M.length; i++) {
-            if (visited[i] == 0){
-                dfs(M, visited, i);
-                count++;
+    public int findCircleNum(int[][] isConnected) {
+        if (isConnected == null || isConnected.length == 0)
+            return 0;
+        
+        int n = isConnected.length;
+        
+        uf a = new uf(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(isConnected[i][j] == 1)
+                    a.union(i, j);
             }
         }
-        return count;
+        return a.getCount();
     }
 }
