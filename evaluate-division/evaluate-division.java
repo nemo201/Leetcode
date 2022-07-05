2ec07e86471ff449cc3fec0a51271816e9717e04
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-05 17:31:32 +0530

    Time: 2 ms, Memory: 42.6 MB - LeetHub

--- evaluate-division/evaluate-division.java
+++ evaluate-division/evaluate-division.java
@@ -1,76 +1,78 @@
-public class Pair{
-    int index;
-    double val;
-    public Pair(int index,double val){
-        this.index = index;
-        this.val = val;
-    }
-}
 class Solution {
-LinkedList<Pair>[] adj;
-public double DFS(int index,int end,boolean[] tracker){
-    //If pointer reached to the destination then return 1
-    if(index==end){
-        return 1;
-    }
-    double ans = -1;
-    tracker[index]=true;
-    Iterator<Pair> i = adj[index].listIterator();
-    while(i.hasNext()){
-        Pair n = i.next();
-        
-        if(!tracker[n.index]){
-            double t = DFS(n.index,end,tracker);
-            if(t>=0){
-                ans=t*n.val;
+
+    public double[] calcEquation(List<List<String>> equations, double[] values,
+            List<List<String>> queries) {
+
+        HashMap<String, Pair<String, Double>> gidWeight = new HashMap<>();
+
+        // Step 1). build the union groups
+        for (int i = 0; i < equations.size(); i++) {
+            List<String> equation = equations.get(i);
+            String dividend = equation.get(0), divisor = equation.get(1);
+            double quotient = values[i];
+
+            union(gidWeight, dividend, divisor, quotient);
+        }
+
+        // Step 2). run the evaluation, with "lazy" updates in find() function
+        double[] results = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            List<String> query = queries.get(i);
+            String dividend = query.get(0), divisor = query.get(1);
+
+            if (!gidWeight.containsKey(dividend) || !gidWeight.containsKey(divisor))
+                // case 1). at least one variable did not appear before
+                results[i] = -1.0;
+            else {
+                Pair<String, Double> dividendEntry = find(gidWeight, dividend);
+                Pair<String, Double> divisorEntry = find(gidWeight, divisor);
+
+                String dividendGid = dividendEntry.getKey();
+                String divisorGid = divisorEntry.getKey();
+                Double dividendWeight = dividendEntry.getValue();
+                Double divisorWeight = divisorEntry.getValue();
+
+                if (!dividendGid.equals(divisorGid))
+                    // case 2). the variables do not belong to the same chain/group
+                    results[i] = -1.0;
+                else
+                    // case 3). there is a chain/path between the variables
+                    results[i] = dividendWeight / divisorWeight;
             }
         }
+
+        return results;
     }
-    return ans;
-}
-public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-    Set<String> set = new HashSet<>();
-    Map<String,Integer> m = new HashMap<>();
-    //Stsore the unique strings
-    for(List<String> i : equations){
-        set.add(i.get(0));
-        set.add(i.get(1));
-    }
-    //Used linkedlist for making the graph
-    adj = new LinkedList[set.size()];
-    //Intialize the linkedlist
-    for(int i=0;i<set.size();i++){
-        adj[i] = new LinkedList();
-    }
-    int in=0;
-    //Indexing each string uniquely
-    for(String i : set){
-        m.put(i,in++);
-    }
-    int j=0;
-    //After indexing the strings, now taking each equation and making the graph(example: (start)->(next,val),(0)->(1,2.0) && (next)->(start,1/val),(1)->(0,1/2.0))
-    for(List<String> i : equations){
-        adj[m.get(i.get(0))].add(new Pair(m.get(i.get(1)),values[j]));
-        adj[m.get(i.get(1))].add(new Pair(m.get(i.get(0)),(1/values[j])));
-        j++;
-        
-    }
-    //Used to store the answer
-    double[] ans = new double[queries.size()];
-    int Qindex = 0;
-    for(List<String> q : queries){
-        Set<String> temp = new HashSet(q);
-        if(set.containsAll(temp)){
-            boolean[] tracker = new boolean[set.size()];
-            //Calling the DFS
-            ans[Qindex]=DFS(m.get(q.get(0)),m.get(q.get(1)),tracker);
-            
+
+    private Pair<String, Double> find(HashMap<String, Pair<String, Double>> gidWeight, String nodeId) {
+        if (!gidWeight.containsKey(nodeId))
+            gidWeight.put(nodeId, new Pair<String, Double>(nodeId, 1.0));
+
+        Pair<String, Double> entry = gidWeight.get(nodeId);
+        // found inconsistency, trigger chain update
+        if (!entry.getKey().equals(nodeId)) {
+            Pair<String, Double> newEntry = find(gidWeight, entry.getKey());
+            gidWeight.put(nodeId, new Pair<String, Double>(
+                    newEntry.getKey(), entry.getValue() * newEntry.getValue()));
         }
-        else{// If any of query string is not present then puting -1
-            ans[Qindex]=-1.0;
-        }
-        Qindex++;
+
+        return gidWeight.get(nodeId);
     }
-    return ans;
-}
+
+    private void union(HashMap<String, Pair<String, Double>> gidWeight, String dividend, String divisor, Double value) {
+        Pair<String, Double> dividendEntry = find(gidWeight, dividend);
+        Pair<String, Double> divisorEntry = find(gidWeight, divisor);
+
+        String dividendGid = dividendEntry.getKey();
+        String divisorGid = divisorEntry.getKey();
+        Double dividendWeight = dividendEntry.getValue();
+        Double divisorWeight = divisorEntry.getValue();
+
+        // merge the two groups together,
+        // by attaching the dividend group to the one of divisor
+        if (!dividendGid.equals(divisorGid)) {
+            gidWeight.put(dividendGid, new Pair<String, Double>(divisorGid,
+                    divisorWeight * value / dividendWeight));
+        }
+    }
 }
