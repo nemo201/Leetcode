3de7b2cdcd6ef86dea4eaaf42854c8c341868dbb
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-08 23:10:06 +0530

    Time: 359 ms, Memory: 232.2 MB - LeetHub

--- find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -10,23 +10,44 @@ class Solution {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
+//         BFS
+//         Queue<Integer> q = new LinkedList<>();
+//         q.offer(source);
+//         Set<Integer> set = new HashSet<>();
+//         set.add(source);
         
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(source);
-        Set<Integer> set = new HashSet<>();
-        set.add(source);
+//         while(!q.isEmpty()) {
+//             int node = q.poll();
+            
+//             if (node == destination)
+//                 return true;
+            
+//             for (int nei : adj.get(node)) {
+                
+//                 if (!set.contains(nei)) {
+//                     set.add(nei);
+//                     q.offer(nei);
+//                 }
+//             }
+//         }
+//         return false;
         
-        while(!q.isEmpty()) {
-            int node = q.poll();
+//         DFS
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> visited = new HashSet();
+        stack.push(source);
+        visited.add(source);
+        
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
             
             if (node == destination)
                 return true;
             
             for (int nei : adj.get(node)) {
-                
-                if (!set.contains(nei)) {
-                    set.add(nei);
-                    q.offer(nei);
+                if (!visited.contains(nei)) {
+                    visited.add(nei);
+                    stack.push(nei);
                 }
             }
         }
