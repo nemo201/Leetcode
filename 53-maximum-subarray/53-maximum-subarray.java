d5641aba683ecc3451855d02aa2b28c7e26a3ad1
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-09 23:23:53 +0530

    Time: 2 ms (70.46%), Space: 72.9 MB (77.77%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,14 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max = 0, ans = Integer.MIN_VALUE;
+        // Initialize our variables using the first element.
+        int currentSubarray = nums[0];
+        int maxSubarray = nums[0];
         
-        for (int n : nums) {
-            max += n;
-            ans = Math.max(ans, max);
-            
-            if (max < 0)
-                max = 0;
+        // Start with the 2nd element since we already used the first one.
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            // If current_subarray is negative, throw it away. Otherwise, keep adding to it.
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
         }
-        return ans;
+        
+        return maxSubarray;
     }
 }
