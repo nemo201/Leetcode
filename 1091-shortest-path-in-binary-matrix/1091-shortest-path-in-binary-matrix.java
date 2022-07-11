c6a48fbb9ba9c5179023c722e5add84358bb6a4a
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-11 13:29:10 +0530

    Time: 35 ms (31.99%), Space: 44 MB (86.54%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,44 +1,47 @@
 class Solution {
-    
-    private static final int[][] directions = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    private static final int[][] dir = new int[][]{{-1, -1}, {-1, 0}, {0, -1}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
     
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if (grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0)
+        if (grid == null || grid.length == 0 || grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0)
             return -1;
         
         Queue<int[]> q = new LinkedList<>();
-        grid[0][0] = 1;
-        q.offer(new int[]{0, 0});
+        q.offer(new int[]{0, 0, 1});
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        visited[0][0] = true;
         
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             int[] cell = q.poll();
             int row = cell[0];
             int col = cell[1];
-            int distance = grid[row][col];
-            if (row == grid.length - 1 && col == grid[0].length - 1) {
-                return distance;
-            }
-            for (int[] neighbour : getNeighbours(row, col, grid)) {
-                int nRow = neighbour[0];
-                int nCol = neighbour[1];
-                q.add(new int[]{nRow, nCol});
-                grid[nRow][nCol] = distance + 1;
+            int dist = cell[2];
+            
+            if (row == grid.length - 1 && col == grid[0].length - 1)
+                return dist;
+            
+            for (int[] nei : getNei(row, col, grid)) {
+                int nRow = nei[0];
+                int nCol = nei[1];
+                
+                if (visited[nRow][nCol])
+                    continue;
+                
+                visited[nRow][nCol] = true;
+                q.offer(new int[]{nRow, nCol, dist + 1});
             }
         }
         return -1;
     }
     
-    private List<int[]> getNeighbours(int row, int col, int[][] grid) {
-        List<int[]> neighbour = new ArrayList<>();
-        for (int i = 0; i < directions.length; i++) {
-            int newRow = row + directions[i][0];
-            int newCol = col + directions[i][1];
-            
+    private List<int[]> getNei(int row, int col, int[][] grid) {
+        List<int[]> nei = new ArrayList<>();
+        for (int i = 0; i < dir.length; i++) {
+            int newRow = row + dir[i][0];
+            int newCol = col + dir[i][1];
             if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length || grid[newRow][newCol] != 0)
                 continue;
-            
-            neighbour.add(new int[]{newRow, newCol});
+            nei.add(new int[]{newRow, newCol});
         }
-        return neighbour;
+        return nei;
     }
 }
