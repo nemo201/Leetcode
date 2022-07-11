61bd79c1931039bf4211b44c6b00fdb8252514f7
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-11 15:27:48 +0530

    Time: 2 ms (46.58%), Space: 42.5 MB (65.67%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,22 +14,47 @@
  * }
  */
 class Solution {
-    List <Integer> list = new ArrayList<>();
     public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
         if (root == null)
             return list;
         
-        dfs(root, 0);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (i == size - 1)
+                    list.add(node.val);
+                
+                if (node.left != null)
+                    q.offer(node.left);
+                if (node.right != null)
+                    q.offer(node.right);
+            }
+        }
         return list;
     }
-    
-    private void dfs(TreeNode root, int level) {
-        if (level == list.size())
-            list.add(root.val);
-        
-        if (root.right != null)
-            dfs(root.right, level + 1);
-        if (root.left != null)
-            dfs(root.left, level + 1);
-    }
 }
+    // DFS
+// class Solution {
+//     List <Integer> list = new ArrayList<>();
+//     public List<Integer> rightSideView(TreeNode root) {
+//         if (root == null)
+//             return list;
+        
+//         dfs(root, 0);
+//         return list;
+//     }
+    
+//     private void dfs(TreeNode root, int level) {
+//         if (level == list.size())
+//             list.add(root.val);
+        
+//         if (root.right != null)
+//             dfs(root.right, level + 1);
+//         if (root.left != null)
+//             dfs(root.left, level + 1);
+//     }
+// }
