ceab0b34752b9767ed6dc2bba3eb37f3c06b9520
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-12 15:25:12 +0530

    Time: 6 ms, Memory: 44 MB - LeetHub

--- rotting-oranges/rotting-oranges.java
+++ rotting-oranges/rotting-oranges.java
@@ -1,19 +1,23 @@
 class Solution {
-    private static final int[][] dir = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private static final int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
     public int orangesRotting(int[][] grid) {
+        if (grid == null || grid.length == 0)
+            return 0;
+        
+        int rows = grid.length, cols = grid[0].length;
+        
         Queue<int[]> q = new LinkedList<>();
-        int ROWS = grid.length, COLS = grid[0].length;
         int fresh = 0;
         
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
                 if (grid[r][c] == 2)
                     q.offer(new int[]{r, c});
                 if (grid[r][c] == 1)
                     fresh++;
             }
         }
-        //level end
         q.offer(new int[]{-1, -1});
         int mins = -1;
         
@@ -22,23 +26,23 @@ class Solution {
             int row = cell[0];
             int col = cell[1];
             
-            if (row == -1){
+            if (row == -1) {
                 mins++;
                 
-                if (!q.isEmpty())
+                if(!q.isEmpty())
                     q.offer(new int[]{-1, -1});
             } else {
                 for (int[] nei : getNei(row, col, grid)) {
                     int nRow = nei[0];
                     int nCol = nei[1];
                     
-                    if (grid[nRow][nCol] == 1){
+                    if (grid[nRow][nCol] == 1) {
                         grid[nRow][nCol] = 2;
                         fresh--;
                         q.offer(new int[]{nRow, nCol});
                     }
                 }
-            } 
+            }
         }
         return fresh == 0 ? mins : -1;
     }
@@ -47,51 +51,13 @@ class Solution {
         List<int[]> list = new ArrayList<>();
         
         for (int i = 0; i < dir.length; i++) {
-            int newRow = row + dir[i][0];
-            int newCol = col + dir[i][1];
+            int newR = row + dir[i][0];
+            int newC = col + dir[i][1];
             
-            if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length)
+            if (newR < 0 || newR >= grid.length || newC < 0 || newC >= grid[0].length)
                 continue;
-            
-            list.add(new int[]{newRow, newCol});
+            list.add(new int[]{newR, newC});
         }
         return list;
     }
 }
-
-// DFS
-// class Solution {
-//     public int orangesRotting(int[][] grid) {
-//         if (grid.length == 0 || grid == null)
-//             return 0;
-        
-//         for (int i = 0; i < grid.length; i++) {
-//             for (int j = 0; j < grid[0].length; j++) {
-//                 if (grid[i][j] == 2)
-//                     dfs(i, j, 2, grid);
-//             }
-//         }
-        
-//         int min = 2;
-        
-//         for (int[] row : grid) {
-//             for (int cell : row) {
-//                 if (cell == 1)
-//                     return -1;
-//                 min = Math.max(min, cell);
-//             }
-//         }
-//         return min - 2;
-//     }
-    
-//     public void dfs(int r, int c, int min, int[][] grid) {
-//         if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0 || 1 < grid[r][c] && grid[r][c] < min)
-//             return;
-        
-//         grid[r][c] = min;
-//         dfs(r + 1, c, min + 1, grid);
-//         dfs(r - 1, c, min + 1, grid);
-//         dfs(r, c + 1, min + 1, grid);
-//         dfs(r, c - 1, min + 1, grid);
-//     }
-// }
