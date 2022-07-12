5a55e132e34d6d9cd6967e970a1983be09b7d186
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-12 17:26:05 +0530

    Time: 945 ms (22.46%), Space: 113.8 MB (49.89%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,30 +1,68 @@
 class Solution {
+    
+    class UnionFind{
+        int[] root;
+        int[] rank;
+        
+        public UnionFind(int size) {
+            root = new int[size];
+            rank = new int[size];
+            
+            for (int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+        
+        public int find(int x) {
+            if (x == root[x])
+                return x;
+            return root[x] = find(root[x]);
+        }
+        
+        public boolean union(int x, int y) {
+            int Rx = find(x);
+            int Ry = find(y);
+            
+            if (Rx == Ry)
+                return false;
+            
+            if (rank[Rx] < rank[Ry])
+                root[Rx] = Ry;
+            else {
+                root[Ry] = Rx;
+                rank[Rx]++;
+            }
+            return true;
+        }
+    }
+    
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        Queue<Pair<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> (a.getKey() - b.getKey()));
+        List<int[]> allEdges = new ArrayList<>();
         
-        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int weight = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                
+                int[] curEdge = {weight, i, j};
+                allEdges.add(curEdge);
+            }
+        }
+        Collections.sort(allEdges, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        UnionFind uf = new UnionFind(n);
         int cost = 0;
-        int edges = 0;
-        heap.add(new Pair(0, 0));
+        int edgesUsed = 0;
         
-        while (edges < n) {
-            Pair<Integer, Integer> top = heap.poll();
-            int weight = top.getKey();
-            int curNode = top.getValue();
+         for (int i = 0; i < allEdges.size() && edgesUsed < n - 1; i++) {
+            int n1 = allEdges.get(i)[1];
+            int n2 = allEdges.get(i)[2];
+            int weight = allEdges.get(i)[0];
             
-            if (visited[curNode])
-                continue;
-            visited[curNode] = true;
-            cost += weight;
-            edges++;
-            
-            for (int nextNode = 0; nextNode < n; nextNode++) {
-                if (!visited[nextNode]) {
-                    int nextWeight = Math.abs(points[curNode][0] - points[nextNode][0]) + Math.abs(points[curNode][1] - points[nextNode][1]);
-                    
-                    heap.add(new Pair(nextWeight, nextNode));
-                }
+            if (uf.union(n1, n2)){
+                cost += weight;
+                edgesUsed++;
             }
         }
         return cost;
