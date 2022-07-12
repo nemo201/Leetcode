617bc67e8cb290c3e8204108aeaeccbd791bce9d
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-12 17:27:40 +0530

    Time: 983 ms, Memory: 119.1 MB - LeetHub

---
+++ min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -0,0 +1,70 @@
+class Solution {
+    
+    class UnionFind{
+        int[] root;
+        int[] rank;
+        
+        public UnionFind(int size) {
+            root = new int[size];
+            rank = new int[size];
+            
+            for (int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+        
+        public int find(int x) {
+            if (x == root[x])
+                return x;
+            return root[x] = find(root[x]);
+        }
+        
+        public boolean union(int x, int y) {
+            int Rx = find(x);
+            int Ry = find(y);
+            
+            if (Rx == Ry)
+                return false;
+            
+            if (rank[Rx] < rank[Ry])
+                root[Rx] = Ry;
+            else {
+                root[Ry] = Rx;
+                rank[Rx]++;
+            }
+            return true;
+        }
+    }
+    
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        List<int[]> allEdges = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int weight = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                
+                int[] curEdge = {weight, i, j};
+                allEdges.add(curEdge);
+            }
+        }
+        Collections.sort(allEdges, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        UnionFind uf = new UnionFind(n);
+        int cost = 0;
+        int edgesUsed = 0;
+        
+         for (int i = 0; i < allEdges.size() && edgesUsed < n - 1; i++) {
+            int n1 = allEdges.get(i)[1];
+            int n2 = allEdges.get(i)[2];
+            int weight = allEdges.get(i)[0];
+            
+            if (uf.union(n1, n2)){
+                cost += weight;
+                edgesUsed++;
+            }
+        }
+        return cost;
+    }
+}
