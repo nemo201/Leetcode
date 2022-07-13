9c1fb156670c6bc7ffd52bdc3d2f541388bf7cc1
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-13 14:36:29 +0530

    Time: 1011 ms, Memory: 108.2 MB - LeetHub

--- min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,37 +1,105 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
+        List<int[]> allEdges = new ArrayList<>();
         
-        Queue<Pair<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a.getKey(), b.getKey()));
-        
-        boolean[] visited = new boolean[n];
-        minHeap.add(new Pair(0, 0));
-        int cost = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int weight = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                int[] curEdge = {weight, i, j};
+                allEdges.add(curEdge);
+            }
+        }
+        Collections.sort(allEdges, (a, b) -> Integer.compare(a[0], b[0]));
         int edgesUsed = 0;
+        int cost = 0;
+        UnionFind uf = new UnionFind(n);
         
-        while (edgesUsed < n) {
-            Pair<Integer, Integer> cell = minHeap.poll();
-            int weight = cell.getKey();
-            int curNode = cell.getValue();
+        for (int i = 0; i < allEdges.size() && edgesUsed < n - 1; i++) {
+            int n1 = allEdges.get(i)[1];
+            int n2 = allEdges.get(i)[2];
+            int weight = allEdges.get(i)[0];
             
-            if (visited[curNode])
-                continue;
-            
-            visited[curNode] = true;
-            cost += weight;
-            edgesUsed++;
-            
-            for (int nextNode = 0; nextNode < n; nextNode++) {
-                if (!visited[nextNode]) {
-                    int nextWeight = Math.abs(points[curNode][0] - points[nextNode][0]) + Math.abs(points[curNode][1] - points[nextNode][1]);
-                    
-                    minHeap.add(new Pair(nextWeight, nextNode));
-                }   
+            if (uf.union(n1, n2)){
+                cost += weight;
+                edgesUsed++;
             }
         }
         return cost;
     }
+    
+    class UnionFind{
+        int[] root;
+        int[] rank;
+        
+        public UnionFind(int size) {
+            root = new int[size];
+            rank = new int[size];
+            
+            for (int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+        
+        public int find(int x) {
+            if (x == root[x])
+                return x;
+            return root[x] = find(root[x]);
+        }
+        
+        public boolean union(int x, int y) {
+            int rx = find(x);
+            int ry = find(y);
+            
+            if (rx == ry)
+                return false;
+            
+            if (rank[rx] < rank[ry]) {
+                root[rx] = ry;
+            } else {
+                root[ry] = rx;
+                rank[rx]++;
+            }
+            return true;
+        }
+    }
 }
+
+// class Solution {
+//     public int minCostConnectPoints(int[][] points) {
+//         int n = points.length;
+        
+//         Queue<Pair<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a.getKey(), b.getKey()));
+        
+//         boolean[] visited = new boolean[n];
+//         minHeap.add(new Pair(0, 0));
+//         int cost = 0;
+//         int edgesUsed = 0;
+        
+//         while (edgesUsed < n) {
+//             Pair<Integer, Integer> cell = minHeap.poll();
+//             int weight = cell.getKey();
+//             int curNode = cell.getValue();
+            
+//             if (visited[curNode])
+//                 continue;
+            
+//             visited[curNode] = true;
+//             cost += weight;
+//             edgesUsed++;
+            
+//             for (int nextNode = 0; nextNode < n; nextNode++) {
+//                 if (!visited[nextNode]) {
+//                     int nextWeight = Math.abs(points[curNode][0] - points[nextNode][0]) + Math.abs(points[curNode][1] - points[nextNode][1]);
+                    
+//                     minHeap.add(new Pair(nextWeight, nextNode));
+//                 }   
+//             }
+//         }
+//         return cost;
+//     }
+// }
 // Kruskal
 // class Solution {
     
