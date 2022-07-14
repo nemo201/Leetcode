164b8b11012130ce537441bc9e1781bb4b9d5c3b
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-14 15:22:20 +0530

    Time: 61 ms, Memory: 68 MB - LeetHub

--- network-delay-time/network-delay-time.java
+++ network-delay-time/network-delay-time.java
@@ -1,36 +1,36 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        
         for (int[] time : times) {
-            map.putIfAbsent(time[0], new HashMap());
+            map.putIfAbsent(time[0], new HashMap<>());
             map.get(time[0]).put(time[1], time[2]);
         }
         
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
-        
-        pq.add(new int[]{0, k});
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+        pq.offer(new int[]{0, k});
         
         boolean[] visited = new boolean[n + 1];
-        int res = 0;
+        int time = 0;
         
-        while(!pq.isEmpty()) {
-            int[] cur = pq.remove();
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int weight = cur[0];
             int curNode = cur[1];
-            int curDist = cur[0];
             
             if (visited[curNode])
                 continue;
             
             visited[curNode] = true;
-            res = curDist;
+            time = weight;
             n--;
             
             if (map.containsKey(curNode)) {
                 for (int next : map.get(curNode).keySet()) {
-                    pq.add(new int[]{curDist + map.get(curNode).get(next), next});
+                    pq.add(new int[]{weight + map.get(curNode).get(next), next});
                 }
             }
         }
-        return n == 0 ? res : -1;
+        return n == 0 ? time : -1;
     }
 }
