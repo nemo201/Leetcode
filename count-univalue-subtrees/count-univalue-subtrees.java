7f6179708895ea471aa131e75bc7f78df4937995
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-15 22:33:02 +0530

    Time: 1 ms, Memory: 42.5 MB - LeetHub

---
+++ count-univalue-subtrees/count-univalue-subtrees.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    public int countUnivalSubtrees(TreeNode root) {
+        if (root == null)
+            return 0;
+        helper(root, 0);
+        return count;
+    }
+    
+    private boolean helper(TreeNode node, int val) {
+        if (node == null)
+            return true;
+        if (!helper(node.left, node.val) | !helper(node.right, node.val))
+            return false;
+        count++;
+        return node.val == val;
+    }
+}
