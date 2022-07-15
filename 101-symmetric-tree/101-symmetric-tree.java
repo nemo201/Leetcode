b7647ebb68f5c7751b9e2880c9feb25ee820de47
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-15 18:10:38 +0530

    Time: 2 ms (12.21%), Space: 43 MB (10.39%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,18 +15,44 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null || root.left == null && root.right == null)
-            return true;
-        return check(root.left, root.right);
-    }
-    
-    public boolean check(TreeNode l, TreeNode r) {
-        if (l == null && r == null)
-            return true;
-        if (l == null || r == null)
-            return false;
-        if (l.val != r.val)
-            return false;
-        return check(l.left, r.right) && check(l.right, r.left);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        q.offer(root);
+        
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            
+            if (t1 == null && t2 == null)
+                continue;
+            if (t1 == null || t2 == null)
+                return false;
+            if (t1.val != t2.val)
+                return false;
+            
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
     }
 }
+// recursive
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         if (root == null || root.left == null && root.right == null)
+//             return true;
+//         return check(root.left, root.right);
+//     }
+    
+//     public boolean check(TreeNode l, TreeNode r) {
+//         if (l == null && r == null)
+//             return true;
+//         if (l == null || r == null)
+//             return false;
+//         if (l.val != r.val)
+//             return false;
+//         return check(l.left, r.right) && check(l.right, r.left);
+//     }
+// }
