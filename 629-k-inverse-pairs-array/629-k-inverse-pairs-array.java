a604d016529256de0657dee59cdedfe0e7b35a27
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-17 22:30:25 +0530

    Time: 25 ms (89.16%), Space: 47.6 MB (72.29%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
@@ -1,20 +1,16 @@
 public class Solution {
     public int kInversePairs(int n, int k) {
-        int[][] dp = new int[n + 1][k + 1];
+        int[] dp = new int[k + 1];
         int M = 1000000007;
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k && j <= i * (i - 1) / 2; j++) {
-                if (i == 1 && j == 0) {
-                    dp[i][j] = 1;
-                    break;
-                } else if (j == 0)
-                    dp[i][j] = 1;
-                else {
-                    int val = (dp[i - 1][j] + M - ((j - i) >= 0 ? dp[i - 1][j - i] : 0)) % M;
-                    dp[i][j] = (dp[i][j - 1] + val) % M;
-                }
+            int[] temp = new int[k + 1];
+            temp[0] = 1;
+            for (int j = 1; j <= k ; j++) {
+                int val = (dp[j] + M - ((j - i) >= 0 ? dp[j - i] : 0)) % M;
+                temp[j] = (temp[j - 1] + val) % M;
             }
+            dp = temp;
         }
-        return dp[n][k];
+        return ((dp[k] + M - (k > 0 ? dp[k - 1] : 0)) % M);
     }
 }
