db012ec7e82b13c1a9940a4277569a9e69b47765
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-19 17:24:27 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (64.96%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -10,17 +10,31 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if (head == null)
+        if (head == null || head.next == null)
             return head;
-        ListNode prevNode = null;
-        ListNode curNode = head;
         
-        while (curNode != null) {
-            ListNode nextNode = curNode.next;
-            curNode.next = prevNode;
-            prevNode = curNode;
-            curNode = nextNode;
-        }
-        return prevNode;
+        ListNode reversedList = reverseList(head.next);
+        head.next.next = head;
+        head.next =null;
+        
+        return reversedList;
     }
 }
+
+// itterrative
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         if (head == null)
+//             return head;
+//         ListNode prevNode = null;
+//         ListNode curNode = head;
+        
+//         while (curNode != null) {
+//             ListNode nextNode = curNode.next;
+//             curNode.next = prevNode;
+//             prevNode = curNode;
+//             curNode = nextNode;
+//         }
+//         return prevNode;
+//     }
+// }
