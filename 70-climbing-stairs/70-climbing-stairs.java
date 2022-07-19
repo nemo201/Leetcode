63b23444b6fed29cc3a214d526f92c2879288bcf
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-20 00:56:25 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (11.03%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,11 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
-        int one = 1, two = 1;
-        for (int i = 0; i < n - 1; i++) {
-            int temp = one;
-            one += two;
-            two = temp;
-        }
-        return one;
+        int[] memo = new int[n + 1];
+        return climbStairs(0, n, memo);
+    }
+    
+    private int climbStairs(int i, int n, int[] memo) {
+        if (i == n)
+            return 1;
+        if (i > n)
+            return 0;
+        if (memo[i] > 0)
+            return memo[i];
+        
+        memo[i] = climbStairs(i + 1, n, memo) + climbStairs(i + 2, n, memo);
+        return memo[i];
     }
 }
