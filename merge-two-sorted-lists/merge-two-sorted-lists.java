bf1737f08b35263f7daf6b93b30370bfab0598f0
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-21 00:08:33 +0530

    Time: 1 ms, Memory: 42 MB - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.java
+++ merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -9,29 +9,48 @@
  * }
  */
 class Solution {
+    
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null)
+        if (list1 == null){
             return list2;
-        if (list2 == null)
-            return list1;
-        
-        ListNode dummy = new ListNode(0);
-        ListNode head = dummy;
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                dummy.next = list1;
-                list1 = list1.next;
-            } else {
-                dummy.next = list2;
-                list2 = list2.next;
-            }
-            dummy = dummy.next;
         }
-        if (list1 != null) 
-            dummy.next = list1;
-        if (list2 != null)
-            dummy.next = list2;
-        
-        return head.next;
+        else if (list2 == null){
+            return list1;
+        }
+        else if (list1.val < list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
     }
 }
+
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         if (list1 == null)
+//             return list2;
+//         if (list2 == null)
+//             return list1;
+        
+//         ListNode dummy = new ListNode(0);
+//         ListNode head = dummy;
+//         while (list1 != null && list2 != null) {
+//             if (list1.val < list2.val) {
+//                 dummy.next = list1;
+//                 list1 = list1.next;
+//             } else {
+//                 dummy.next = list2;
+//                 list2 = list2.next;
+//             }
+//             dummy = dummy.next;
+//         }
+//         if (list1 != null) 
+//             dummy.next = list1;
+//         if (list2 != null)
+//             dummy.next = list2;
+        
+//         return head.next;
+//     }
+// }
