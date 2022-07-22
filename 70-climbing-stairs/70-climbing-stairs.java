d170de9bdec8b912e72c329120abacf125d1fa09
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-22 16:40:43 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (65.22%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,15 +1,32 @@
 class Solution {
-    Map<Integer, Integer> memo = new HashMap<>();
     public int climbStairs(int n) {
-        if (n <= 2)
-            return n;
+        if (n == 1)
+            return 1;
         
-        if (!memo.containsKey(n))
-            memo.put(n, climbStairs(n - 1) + climbStairs(n - 2));
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
         
-        return memo.get(n);
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
+
+// memo
+// class Solution {
+//     Map<Integer, Integer> memo = new HashMap<>();
+//     public int climbStairs(int n) {
+//         if (n <= 2)
+//             return n;
+        
+//         if (!memo.containsKey(n))
+//             memo.put(n, climbStairs(n - 1) + climbStairs(n - 2));
+        
+//         return memo.get(n);
+//     }
+// }
 
 // recurssion
 // class Solution {
