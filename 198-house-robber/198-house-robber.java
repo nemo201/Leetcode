332380021cf2048aa23feae6fd08ae59ad5276d4
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-22 20:42:07 +0530

    Time: 1 ms (20.84%), Space: 41.2 MB (73.45%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,22 +1,41 @@
-//memo
+//bottom up
 class Solution {
-    Map<Integer, Integer> memo = new HashMap<>();
-    int[] nums;
-    
-    private int dp(int i) {
-        //base case
-        if (i == 0)
-            return nums[0];
-        if (i == 1)
-            return Math.max(nums[0], nums[1]);
-        if (!memo.containsKey(i)) {
-            memo.put(i, Math.max(dp(i - 1), dp(i - 2) + nums[i]));
-        }
-        return memo.get(i);
-    }
-    
     public int rob(int[] nums) {
-        this.nums = nums;
-        return dp(nums.length - 1);
+        if (nums.length == 1)
+            return nums[0];
+        
+        int[] dp = new int[nums.length + 1];
+        
+        //base case
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.length - 1];
     }
 }
+
+//memo
+// class Solution {
+//     Map<Integer, Integer> memo = new HashMap<>();
+//     int[] nums;
+    
+//     private int dp(int i) {
+//         //base case
+//         if (i == 0)
+//             return nums[0];
+//         if (i == 1)
+//             return Math.max(nums[0], nums[1]);
+//         if (!memo.containsKey(i)) {
+//             memo.put(i, Math.max(dp(i - 1), dp(i - 2) + nums[i]));
+//         }
+//         return memo.get(i);
+//     }
+    
+//     public int rob(int[] nums) {
+//         this.nums = nums;
+//         return dp(nums.length - 1);
+//     }
+// }
