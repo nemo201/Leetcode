5af2f62acf640576d4a1706267feb9fe76428ded
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-23 17:19:05 +0530

    Time: 25 ms (15.07%), Space: 47.7 MB (55.12%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,29 +1,51 @@
 class Solution {
-    private Map<Integer, Integer> points = new HashMap<>();
-    private Map<Integer, Integer> cache = new HashMap<>();
-    
     public int deleteAndEarn(int[] nums) {
+        Map<Integer, Integer> points = new HashMap<>();
         int maxNum = 0;
+        
         for (int num : nums) {
             points.put(num, points.getOrDefault(num, 0) + num);
             maxNum = Math.max(maxNum, num);
         }
-        return maxPoints(maxNum);        
-    }
-    
-    private int maxPoints(int num) {
-        if (num == 0)
-            return 0;
         
-        if (num == 1)
-            return points.getOrDefault(1, 0);
+        int[] dp = new int[maxNum + 1];
+        dp[1] = points.getOrDefault(1, 0);
         
-        if (cache.containsKey(num))
-            return cache.get(num);
-        
-        int gain = points.getOrDefault(num, 0);
-        cache.put(num, Math.max(maxPoints(num - 1), maxPoints(num - 2) + gain));
-        
-        return cache.get(num);
+        for (int num = 2; num < dp.length; num++) {
+            int gain = points.getOrDefault(num, 0);
+            dp[num] = Math.max(dp[num - 1], dp[num - 2] + gain);
+        }
+        return dp[maxNum];
     }
 }
+
+// top down
+// class Solution {
+//     private Map<Integer, Integer> points = new HashMap<>();
+//     private Map<Integer, Integer> cache = new HashMap<>();
+    
+//     public int deleteAndEarn(int[] nums) {
+//         int maxNum = 0;
+//         for (int num : nums) {
+//             points.put(num, points.getOrDefault(num, 0) + num);
+//             maxNum = Math.max(maxNum, num);
+//         }
+//         return maxPoints(maxNum);        
+//     }
+    
+//     private int maxPoints(int num) {
+//         if (num == 0)
+//             return 0;
+        
+//         if (num == 1)
+//             return points.getOrDefault(1, 0);
+        
+//         if (cache.containsKey(num))
+//             return cache.get(num);
+        
+//         int gain = points.getOrDefault(num, 0);
+//         cache.put(num, Math.max(maxPoints(num - 1), maxPoints(num - 2) + gain));
+        
+//         return cache.get(num);
+//     }
+// }
