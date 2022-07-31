6d433ff98a25770031e5b8076db295b89e48fb67
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-07-31 16:48:15 +0530

    Time: 1 ms (41.06%), Space: 41.9 MB (39.45%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.java
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.java
@@ -13,22 +13,20 @@
  *     }
  * }
  */
-class Solution {
+class Solution {    
     public TreeNode increasingBST(TreeNode root) {
-        List<Integer> vals = new ArrayList<>();
+        List<Integer> vals = new ArrayList();
         inorder(root, vals);
         TreeNode ans = new TreeNode(0), cur = ans;
-        
-        for (int v : vals) {
+        for (int v: vals) {
             cur.right = new TreeNode(v);
             cur = cur.right;
         }
         return ans.right;
     }
-    
+
     public void inorder(TreeNode node, List<Integer> vals) {
-        if (node == null)
-            return;
+        if (node == null) return;
         inorder(node.left, vals);
         vals.add(node.val);
         inorder(node.right, vals);
