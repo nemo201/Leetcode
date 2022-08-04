fae06db7561706bb0032ce689084155aadc18bd9
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-08-05 01:16:20 +0530

    Time: 10 ms (8.11%), Space: 42.8 MB (51.56%) - LeetHub

--- 284-peeking-iterator/284-peeking-iterator.java
+++ 284-peeking-iterator/284-peeking-iterator.java
@@ -2,45 +2,41 @@
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 
 import java.util.NoSuchElementException;
+
 class PeekingIterator implements Iterator<Integer> {
-    Integer next;
-    Iterator<Integer> iter;
-    boolean noSuchElement;
-
-    public PeekingIterator(Iterator<Integer> iterator) {
-	// initialize any member here.
-	iter = iterator;
-        advanceIter();
-    }
-
-    // Returns the next element in the iteration without advancing the iterator.
-    public Integer peek() {
-        // you should confirm with interviewer what to return/throw
-        // if there are no more values
-        return next;
-    }
-
-    // hasNext() and next() should behave the same as in the Iterator interface.
-    // Override them if needed.
-    @Override
-    public Integer next() {
-        if (noSuchElement)
-            throw new NoSuchElementException();
-        Integer res = next;
-        advanceIter();
-        return res;
-    }
-
-    @Override 
-    public boolean hasNext() {
-        return !noSuchElement;
-    }
+    private Iterator<Integer> iter;
+    private Integer next = null;
     
-    private void advanceIter() {
-        if (iter.hasNext()) {
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    // initialize any member here.
+        if (iterator.hasNext())
+            next = iterator.next();
+        iter = iterator;
+	    
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+        return next;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+	    if (next == null)
+            throw new NoSuchElementException();
+        
+        Integer toReturn = next;
+        next = null;
+        if (iter.hasNext())
             next = iter.next();
-        } else {
-            noSuchElement = true;
-        }
-    }
+        
+        return toReturn;
+	}
+	
+	@Override
+	public boolean hasNext() {
+	    return next != null;
+	}
 }
