58c7d8e7880877324fe02cb2e37b03d39de39d3a
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-08-08 23:13:14 +0530

    Time: 5 ms (63.86%), Space: 42.5 MB (90.66%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -15,31 +15,37 @@
  */
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
-        dfs(root, 0, 0, map);
-        List<List<Integer>> list = new ArrayList<>();
-        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
-            list.add(new ArrayList<>());
-            for (PriorityQueue<Integer> nodes : ys.values()) {
-                while (!nodes.isEmpty()) {
-                    list.get(list.size() - 1).add(nodes.poll());
-                }
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null)
+            return ans;
+        Queue<int[]> q = new PriorityQueue<>((a, b) -> {
+            if (a[1] == b[1]) {
+                if (a[0] == b[0])
+                    return Integer.compare(a[2], b[2]);
+                return Integer.compare(a[0], b[0]);
             }
+            return Integer.compare(a[1], b[1]);
+        });
+        helper(root, 0, 0, q);
+        
+        while (!q.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            int[] node = q.poll();
+            temp.add(node[2]);
+            while (!q.isEmpty() && q.peek()[1] == node[1]) {
+                int[] tmp = q.poll();
+                temp.add(tmp[2]);
+            }
+            ans.add(temp);
         }
-        return list;
+        return ans;
     }
-    private void dfs(TreeNode root, int x, int y, TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map) {
-        if (root == null) {
+    
+    private void helper(TreeNode node, int row, int col, Queue<int[]> q) {
+        if (node == null)
             return;
-        }
-        if (!map.containsKey(x)) {
-            map.put(x, new TreeMap<>());
-        }
-        if (!map.get(x).containsKey(y)) {
-            map.get(x).put(y, new PriorityQueue<>());
-        }
-        map.get(x).get(y).offer(root.val);
-        dfs(root.left, x - 1, y + 1, map);
-        dfs(root.right, x + 1, y + 1, map);
+        helper(node.left, row + 1, col - 1, q);
+        q.offer(new int[] {row, col, node.val});
+        helper(node.right, row + 1, col + 1, q);
     }
 }
