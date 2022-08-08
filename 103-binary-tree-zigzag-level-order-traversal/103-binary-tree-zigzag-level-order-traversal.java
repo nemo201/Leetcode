73e4613707c8568784148f7009839fd8e83e750c
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-08-08 14:10:21 +0530

    Time: 1 ms (96.41%), Space: 43 MB (26.92%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -15,31 +15,34 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new LinkedList<>();
+        LinkedList<List<Integer>> ans = new LinkedList<>();
         if (root == null)
             return ans;
-        
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
+        
         boolean even = true;
+        
         while (!q.isEmpty()) {
             int size = q.size();
             LinkedList<Integer> curLevel = new LinkedList<>();
+            
             for (int i = 0; i < size; i++) {
-                TreeNode curNode = q.poll();
+                TreeNode node = q.poll();
                 
-                if (curNode.left != null)
-                    q.offer(curNode.left);
-                if (curNode.right != null)
-                    q.offer(curNode.right);
+                if (node.left != null)
+                    q.offer(node.left);
+                if (node.right != null)
+                    q.offer(node.right);
                 
                 if (even) {
-                    curLevel.add(curNode.val);
-                } else 
-                    curLevel.addFirst(curNode.val);
+                    curLevel.add(node.val);
+                } else {
+                    curLevel.addFirst(node.val);
+                }
             }
-            ans.add(curLevel);
             even = !even;
+            ans.add(curLevel);
         }
         return ans;
     }
