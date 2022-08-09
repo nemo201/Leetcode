f7789b1cc9d239557d4d4f4bb2ea80fb0842e468
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-08-09 12:53:24 +0530

    Time: 30 ms (87.15%), Space: 107.6 MB (55.06%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -16,20 +16,20 @@
 class Solution {
     public String getDirections(TreeNode root, int startValue, int destValue) {
         StringBuilder s = new StringBuilder(), d = new StringBuilder();
-        
         helper(root, startValue, s);
         helper(root, destValue, d);
         
-        int i = 0, max_i = Math.min(s.length(), d.length());
-        while (i < max_i && s.charAt(s.length() - i - 1) == d.charAt(d.length() - i - 1))
+        int i = 0, i_max = Math.min(s.length(), d.length());
+        
+        while (i < i_max && s.charAt(s.length() - i - 1) == d.charAt(d.length() - i - 1))
             i++;
+        
         return "U".repeat(s.length() - i) + d.reverse().toString().substring(i);
     }
     
     private boolean helper(TreeNode node, int val, StringBuilder sb) {
         if (node.val == val)
             return true;
-        
         if (node.left != null && helper(node.left, val, sb))
             sb.append("L");
         else if (node.right != null && helper(node.right, val, sb))
