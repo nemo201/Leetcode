7653163a0f05dc93535d14ef832f20d976700451
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-08-11 20:56:31 +0530

    Time: 1 ms (62.14%), Space: 43.7 MB (69.53%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -16,15 +16,15 @@
 class Solution {
     Integer prev = null;
     public boolean isValidBST(TreeNode root) {
-        prev = null;
         return helper(root);
     }
+    
     private boolean helper(TreeNode node) {
         if (node == null)
             return true;
         if (!helper(node.left))
             return false;
-        if (prev != null && node.val <= prev)
+        if (prev != null && prev >= node.val)
             return false;
         prev = node.val;
         return helper(node.right);
