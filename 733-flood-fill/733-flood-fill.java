3eca73e00dc8bbf7a7e8ec3c08e75056d0b221e3
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-08-11 23:22:58 +0530

    Time: 1 ms (90.58%), Space: 48.1 MB (50.05%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,23 +1,20 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newCol) {
         int color = image[sr][sc];
-        if (color != newColor)
-            dfs (image, sr, sc, color, newColor);
-        
+        if (color != newCol) {
+            helper(image, sr, sc, color, newCol);
+        }
         return image;
     }
     
-    void dfs(int[][] image, int r, int c, int color, int newColor) {
-        if (image[r][c] == color) {
-            image[r][c] = newColor;
-            if (r >= 1)
-                dfs(image, r - 1, c, color, newColor);
-            if (c >= 1)
-                dfs(image, r , c - 1, color, newColor);
-            if (r + 1 <image.length)
-                dfs(image, r + 1, c, color, newColor);
-            if (c + 1 <image[0].length)
-                dfs(image, r , c + 1, color, newColor);
-        }
+    private void helper(int[][] image, int row, int col, int color, int newCol) {
+        if (row < 0 || row >= image.length || col < 0 || col >= image[0].length || image[row][col] != color)
+            return;
+        
+        image[row][col] = newCol;    
+        helper(image, row + 1, col, color, newCol);
+        helper(image, row, col + 1, color, newCol);
+        helper(image, row - 1, col, color, newCol);
+        helper(image, row, col - 1, color, newCol);
     }
 }
