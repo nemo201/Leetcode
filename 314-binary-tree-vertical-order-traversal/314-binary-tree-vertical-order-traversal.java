608ff724a869cbe0b49e949df8bbf803786ccf82
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-08-14 19:33:02 +0530

    Time: 7 ms (24.77%), Space: 43.5 MB (58.69%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.java
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.java
@@ -16,6 +16,7 @@
 class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
+        
         if (root == null)
             return ans;
         
@@ -29,17 +30,17 @@ class Solution {
             root = p.getKey();
             col = p.getValue();
             
-            if (!colTable.containsKey(col))
+            if (!colTable.containsKey(col)) {
                 colTable.put(col, new ArrayList());
+            }
             colTable.get(col).add(root.val);
             
             if (root.left != null)
                 q.offer(new Pair(root.left, col - 1));
-            
             if (root.right != null)
                 q.offer(new Pair(root.right, col + 1));
         }
-        List<Integer> sorted = new ArrayList<>(colTable.keySet());
+        List<Integer> sorted = new ArrayList(colTable.keySet());
         Collections.sort(sorted);
         
         for (int k : sorted)
