4623e3160cb7c537a8dc5433bb6a1d8338e069fc
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-08-17 00:22:38 +0530

    Time: 4 ms (82.57%), Space: 42.7 MB (84.20%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -19,32 +19,33 @@ class Solution {
         
         Queue<int[]> q = new PriorityQueue<>((a, b) -> {
             if (a[1] == b[1]) {
-                if (a[0] == b[0])
+                if (a[0] == b[0]) {
                     return Integer.compare(a[2], b[2]);
+                }
                 return Integer.compare(a[0], b[0]);
             }
             return Integer.compare(a[1], b[1]);
         });
-        helper(root, 0, 0, q);
         
+        dfs(root, 0, 0, q);
         while (!q.isEmpty()) {
-            List<Integer> layer = new ArrayList<>();
+            List<Integer> layer = new ArrayList();
             int[] node = q.poll();
             layer.add(node[2]);
             while (!q.isEmpty() && q.peek()[1] == node[1]) {
-                int[] tmp = q.poll();
-                layer.add(tmp[2]);
+                int[] temp = q.poll();
+                layer.add(temp[2]);
             }
             ans.add(layer);
         }
         return ans;
     }
     
-    private void helper(TreeNode node, int row, int col, Queue<int[]> q) {
+    private void dfs(TreeNode node, int row, int col, Queue<int[]> q) {
         if (node == null)
             return;
-        helper(node.left, row + 1, col - 1, q);
+        dfs(node.left, row + 1, col - 1, q);
         q.offer(new int[]{row, col, node.val});
-        helper(node.right, row + 1, col + 1, q);
+        dfs(node.right, row + 1, col + 1, q);
     }
 }
