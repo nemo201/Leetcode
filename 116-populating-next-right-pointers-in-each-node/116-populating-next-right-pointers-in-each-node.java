75deefe4ec068c7f2f10f2d90d908c88aafba1db
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-08-24 21:12:45 +0530

    Time: 1 ms (69.66%), Space: 46.7 MB (82.92%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -24,7 +24,8 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         if (root == null)
-            return null;
+            return root;
+        
         Node leftMost = root;
         while (leftMost.left != null) {
             Node head = leftMost;
@@ -32,9 +33,9 @@ class Solution {
             while (head != null) {
                 head.left.next = head.right;
                 
-                if (head.next != null) {
+                if (head.next != null)
                     head.right.next = head.next.left;
-                }
+                
                 head = head.next;
             }
             leftMost = leftMost.left;
