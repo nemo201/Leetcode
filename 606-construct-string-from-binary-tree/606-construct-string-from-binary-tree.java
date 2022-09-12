477cdfda045b9290e1b8c3da0b49cc383e8131f3
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-09-12 20:41:52 +0530

    Time: 38 ms (23.51%), Space: 54.6 MB (33.49%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
@@ -19,10 +19,8 @@ class Solution {
             return "";
         if (root.left == null && root.right == null)
             return root.val + "";
-        if (root.right == null) {
+        if (root.right == null)
             return root.val + "(" + tree2str(root.left) + ")";
-        }
-        
-        return root.val + "(" + tree2str(root.left)+")("+tree2str(root.right)+")";  
+        return root.val + "(" + tree2str(root.left) + ")(" + tree2str(root.right) + ")";
     }
 }
