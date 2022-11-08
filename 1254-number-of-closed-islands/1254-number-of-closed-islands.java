8a6a5048c72bf622ed802c01168daeb35585c848
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-11-09 00:03:35 +0530

    Time: 2 ms (96.18%), Space: 42.6 MB (95.76%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,30 +1,30 @@
 class Solution {
-    int[] directions = new int[] {0, 1, 0, -1, 0};
     public int closedIsland(int[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (i == 0 || j == 0 || i == grid.length - 1 || j == grid[0].length - 1)
-                    fill(grid, i, j);
-            }
-        }
+        int ans = 0;
         
-        int res = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 0) {
-                    res++;
-                    fill(grid, i, j);
+                    if (dfs(grid, i, j))
+                        ans++;
                 }
             }
         }
-        return res;
+        return ans;
     }
     
-    void fill(int[][] grid, int x, int y) {
-        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] == 1)
-            return;
-        grid[x][y] = 1;
-        for (int i = 0; i < directions.length - 1; i++)
-            fill(grid, x + directions[i], y + directions[i + 1]);
+    private boolean dfs(int[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length)
+            return false;
+        
+        if (grid[row][col] == 1)
+            return true;
+        
+        grid[row][col] = 1;
+        
+        boolean res = true;
+        
+        res = res & dfs(grid, row + 1, col) & dfs(grid, row, col + 1) & dfs(grid, row - 1, col) & dfs(grid, row, col - 1);
+        return res;
     }
 }
