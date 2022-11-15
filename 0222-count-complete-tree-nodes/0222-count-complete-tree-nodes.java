355615a14af101c89f3d18a8c8b932c59e27b42f
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2022-11-15 16:15:11 +0530

    Time: 0 ms (100.00%), Space: 50.6 MB (9.89%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -15,6 +15,50 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-    return root != null ? 1 + countNodes(root.right) + countNodes(root.left) : 0;
+    if (root == null)
+        return 0;
+        
+        int d = computeDepth(root);
+        
+        if (d == 0)
+            return 1;
+        
+        int left = 1, right = (int)Math.pow(2, d) - 1;
+        int pivot;
+        
+        while (left <= right) {
+            pivot = left + (right - left) / 2;
+            
+            if (exists(pivot, d, root))
+                left = pivot + 1;
+            else
+                right = pivot - 1;
+        }
+        return (int)Math.pow(2, d) - 1 + left;
   }
+    public int computeDepth(TreeNode node) {
+        int d = 0;
+        while (node.left != null){
+            node = node.left;
+            d++;
+        }
+        return d;
+    }
+    
+    public boolean exists(int idx, int d, TreeNode node) {
+        int left = 0, right = (int)Math.pow(2, d) - 1;
+        int pivot;
+        
+        for (int i = 0; i < d; i++) {
+            pivot = left + (right - left) / 2;
+            if (idx <= pivot){
+                node = node.left;
+                right = pivot;
+            } else {
+                node = node.right;
+                left = pivot + 1;
+            }
+        }
+        return node != null;
+    }
 }
