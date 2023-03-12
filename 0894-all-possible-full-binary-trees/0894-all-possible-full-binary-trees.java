c394af318149f88d9fe57ea44ca495b1f122da88
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2023-03-12 16:53:33 +0530

    Time: 1 ms (100.00%), Space: 44.6 MB (43.84%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
@@ -14,27 +14,29 @@
  * }
  */
 class Solution {
+    Map<Integer, List<TreeNode>> map = new HashMap<>();
     public List<TreeNode> allPossibleFBT(int n) {
-        List<TreeNode> ans = new ArrayList();
-        
-        if (n == 1) {
-            List<TreeNode> base = new ArrayList();
-            base.add(new TreeNode(0));
-            return base;
-        }
-        
-        for (int i = 1; i < n; i += 2) {
-            List<TreeNode> left = allPossibleFBT(i);
-            List<TreeNode> right = allPossibleFBT(n - i - 1);
-            for (TreeNode l : left) {
-                for (TreeNode r : right) {
-                    TreeNode root = new TreeNode(0);
-                    root.left = l;
-                    root.right = r;
-                    ans.add(root);
+        if (!map.containsKey(n)) {
+            if (n == 1) {
+                List<TreeNode> base = new ArrayList();
+                base.add(new TreeNode(0));
+                return base;
+            }
+            List<TreeNode> ans = new ArrayList();
+            for (int i = 1; i < n; i += 2) {
+                List<TreeNode> left = allPossibleFBT(i);
+                List<TreeNode> right = allPossibleFBT(n - i - 1);
+                for (TreeNode l : left) {
+                    for (TreeNode r : right) {
+                        TreeNode root = new TreeNode(0);
+                        root.left = l;
+                        root.right = r;
+                        ans.add(root);
+                    }
                 }
             }
+            map.put(n, ans);
         }
-        return ans;
+        return map.get(n);
     }
 }
