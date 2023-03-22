01fc8dcdf4aec403dbcae81f8b0212dfa74f9063
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2023-03-22 14:47:49 +0530

    Time: 115 ms (24.10%), Space: 88 MB (99.64%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,26 +1,37 @@
 class Solution {
-    int min = Integer.MAX_VALUE;
+    
     public int minScore(int n, int[][] roads) {
         Map<Integer, List<List<Integer>>> adj = new HashMap<>();
         for (int[] road : roads) {
             adj.computeIfAbsent(road[0], k -> new ArrayList<List<Integer>>()).add(Arrays.asList(road[1], road[2]));
             adj.computeIfAbsent(road[1], k -> new ArrayList<List<Integer>>()).add(Arrays.asList(road[0], road[2]));
         }
-        boolean[] visit = new boolean[n + 1];
-        dfs(1, adj, visit);
-        return min;
+        
+        return bfs(n, adj);
     }
     
-    private void dfs(int n, Map<Integer, List<List<Integer>>> adj, boolean[] visit) {
-        visit[n] = true;
+    private int bfs(int n, Map<Integer, List<List<Integer>>> adj) {
+        boolean[] visit = new boolean[n + 1];
+        Queue<Integer> q = new LinkedList();
+        int ans = Integer.MAX_VALUE;
         
-        if (!adj.containsKey(n))
-            return;
+        q.offer(1);
+        visit[1] = true;
         
-        for (List<Integer> edge : adj.get(n)) {
-            min = Math.min(min, edge.get(1));
-            if (!visit[edge.get(0)])
-                dfs(edge.get(0), adj, visit);
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            
+            if (!adj.containsKey(node))
+                continue;
+            
+            for (List<Integer> edge : adj.get(node)) {
+                ans = Math.min(ans, edge.get(1));
+                if (!visit[edge.get(0)]) {
+                    visit[edge.get(0)] = true;
+                    q.offer(edge.get(0));
+                }
+            }
         }
+        return ans;
     }
 }
