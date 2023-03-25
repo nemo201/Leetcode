272a918663bc372f666f834f57426b5680d0e200
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2023-03-25 14:14:57 +0530

    Time: 33 ms (82.38%), Space: 108.9 MB (70.50%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -1,25 +1,48 @@
 class Solution {
     public long countPairs(int n, int[][] edges) {
-        Map<Integer, List<Integer>> adj = new HashMap<>();
+//         Map<Integer, List<Integer>> adj = new HashMap<>();
+//         for (int[] edge : edges) {
+//             adj.computeIfAbsent(edge[0], k -> new ArrayList<Integer>()).add(edge[1]);
+//             adj.computeIfAbsent(edge[1], k -> new ArrayList<Integer>()).add(edge[0]);
+//         }
+        
+//         boolean[] visited = new boolean[n];
+//         long pairs = 0;
+//         long sizeComponent = 0;
+//         long remaining = n;
+        
+//         for (int i = 0; i < n; i++) {
+//             if (!visited[i]) {
+//                 // sizeComponent = dfs(i, adj, visited);
+//                 sizeComponent = bfs(i, adj, visited);
+//                 pairs += sizeComponent * (remaining - sizeComponent);
+//                 remaining -= sizeComponent;
+//             }
+//         }
+//         return pairs;
+        
+        UnionFind uf = new UnionFind(n);
+        
         for (int[] edge : edges) {
-            adj.computeIfAbsent(edge[0], k -> new ArrayList<Integer>()).add(edge[1]);
-            adj.computeIfAbsent(edge[1], k -> new ArrayList<Integer>()).add(edge[0]);
+            uf.union(edge[0], edge[1]);
         }
         
-        boolean[] visited = new boolean[n];
-        long pairs = 0;
-        long sizeComponent = 0;
-        long remaining = n;
+        Map<Integer, Integer> componentSize = new HashMap<>();
         
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                // sizeComponent = dfs(i, adj, visited);
-                sizeComponent = bfs(i, adj, visited);
-                pairs += sizeComponent * (remaining - sizeComponent);
-                remaining -= sizeComponent;
-            }
+            int parent = uf.find(i);
+            if (componentSize.containsKey(parent))
+                componentSize.put(parent, componentSize.get(parent) + 1);
+            else
+                componentSize.put(parent, 1);
         }
-        return pairs;
+        long numberOfPaths = 0;
+        long remainingNodes = n;
+        for (int component : componentSize.values()) {
+            numberOfPaths += component * (remainingNodes - component);
+            remainingNodes -= component;
+        }
+        return numberOfPaths;
     }
     public int dfs(int node, Map<Integer, List<Integer>> adj, boolean[] visited) {
         int count = 1;
@@ -58,5 +81,38 @@ class Solution {
             }
         }
         return count;
+    }
+}
+
+class UnionFind {
+    int[] root;
+    int[] rank;
+    
+    public UnionFind(int size) {
+        root = new int[size];
+        rank = new int[size];
+        
+        for (int i = 0; i < size; i++)
+            root[i] = i;
+    }
+    
+    public int find(int x) {
+        if (root[x]!= x)
+            root[x] = find(root[x]);
+        return root[x];
+    }
+    
+    public void union(int x, int y) {
+        int rx = find(x);
+        int ry = find(y);
+        
+        if (rx == ry)
+            return;
+        else if (rank[rx] < rank[ry])
+            root[rx] = ry;
+        else {
+            root[ry] = rx;
+            rank[rx]++;
+        }
     }
 }
