30197a7f2eb27c9d129c48579d0bfc8ddae4c119
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2023-03-26 21:22:21 +0530

    Time: 26 ms (85.71%), Space: 60.6 MB (79.92%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -1,27 +1,45 @@
 class Solution {
-    int answer = -1;
-
-    public void dfs(int node, int[] edges, Map<Integer, Integer> dist, boolean[] visit) {
-        visit[node] = true;
-        int neighbor = edges[node];
-
-        if (neighbor != -1 && !visit[neighbor]) {
-            dist.put(neighbor, dist.get(node) + 1);
-            dfs(neighbor, edges, dist, visit);
-        } else if (neighbor != -1 && dist.containsKey(neighbor)) {
-            answer = Math.max(answer, dist.get(node) - dist.get(neighbor) + 1);
-        }
-    }
-
     public int longestCycle(int[] edges) {
         int n = edges.length;
         boolean[] visit = new boolean[n];
-
+        int[] indegree = new int[n];
+        
+        for (int edge : edges) {
+            if (edge != -1)
+                indegree[edge]++;
+        }
+        
+        Queue<Integer> q = new LinkedList();
+        
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.offer(i);
+        }
+        
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            visit[node] = true;
+            int nei = edges[node];
+            if (nei != -1) {
+                indegree[nei]--;
+                if (indegree[nei] == 0)
+                    q.offer(nei);
+            }
+        }
+        
+        int answer = -1;
         for (int i = 0; i < n; i++) {
             if (!visit[i]) {
-                Map<Integer, Integer> dist = new HashMap<>();
-                dist.put(i, 1);
-                dfs(i, edges, dist, visit);
+                int nei = edges[i];
+                int count = 1;
+                visit[i] = true;
+                // Iterate in the cycle.
+                while (nei != i) {
+                    visit[nei] = true;
+                    count++;
+                    nei = edges[nei];
+                }
+                answer = Math.max(answer, count);
             }
         }
         return answer;
