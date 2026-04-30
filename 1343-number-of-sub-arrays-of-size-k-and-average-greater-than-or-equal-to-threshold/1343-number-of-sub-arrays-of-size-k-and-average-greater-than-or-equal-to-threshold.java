0c135af821103474ac67beb5051991f192d8e23a
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-04-30 13:11:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,12 +1,13 @@
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
         int sum = 0;
+        int target = k * threshold;
         for (int i = 0; i < k; i++) {
             sum += arr[i];
         }
 
         int count = 0;
-        if ((double) sum / k >= threshold) {
+        if (sum >= target) {
             count++;
         }
 
@@ -14,7 +15,7 @@ class Solution {
             sum += arr[i];
             sum -= arr[i - k];
 
-            if ((double) sum / k >= threshold) {
+            if (sum >= target) {
                 count++;
             }
         }
