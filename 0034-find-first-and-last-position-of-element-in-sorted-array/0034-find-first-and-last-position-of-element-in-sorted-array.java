50b10a7367dfd385130ba19e015a7ea51c77885e
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-05-07 23:16:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,29 +1,51 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+
+        if (first == -1) {
+            return new int[]{-1, -1};
+        }
+
+        int last = findLast(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
+        int ans = -1;
 
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
+            int mid = left + (right - left)/ 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
                 right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             } else {
-                int[] ans = new int[2];
-                int i = mid;
-                while (i >= 0 && nums[i] == target) {
-                    i--;
-                }
-                ans[0] = i + 1;
-                i = mid;
-                while (i < nums.length &&nums[i] == target) {
-                    i++;
-                }
-                ans[1] = i - 1;
-                return ans;
+                right = mid - 1;
             }
         }
-        return new int[]{-1, -1};
+        return ans;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left)/ 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
     }
 }
