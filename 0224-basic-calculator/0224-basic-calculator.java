1430efb777e1898d06fc3b8e3b840168cff1f8ab
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-05-20 13:13:20 +0530

    Time: 12 ms (36.34%), Space: 46.2 MB (91.02%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,68 +1,37 @@
 class Solution {
     public int calculate(String s) {
-
-        Stack<Integer> stack = new Stack<Integer>();
-        int operand = 0;
-        int result = 0; // For the on-going result
-        int sign = 1;  // 1 means positive, -1 means negative
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int result = 0;
+        int number = 0;
+        int sign = 1;
 
         for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
 
-            char ch = s.charAt(i);
-            if (Character.isDigit(ch)) {
-
-                // Forming operand, since it could be more than one digit
-                operand = 10 * operand + (int) (ch - '0');
-
-            } else if (ch == '+') {
-
-                // Evaluate the expression to the left,
-                // with result, sign, operand
-                result += sign * operand;
-
-                // Save the recently encountered '+' sign
+            if (Character.isDigit(c)) {
+                number = number * 10 + (c - '0');
+            } else if (c == '+') {
+                result += sign * number;
+                number = 0;
                 sign = 1;
-
-                // Reset operand
-                operand = 0;
-
-            } else if (ch == '-') {
-
-                result += sign * operand;
+            } else if (c == '-') {
+                result += sign * number;
+                number = 0;
                 sign = -1;
-                operand = 0;
-
-            } else if (ch == '(') {
-
-                // Push the result and sign on to the stack, for later
-                // We push the result first, then sign
+            } else if (c == '(') {
                 stack.push(result);
                 stack.push(sign);
-
-                // Reset operand and result, as if new evaluation begins for the new sub-expression
-                sign = 1;
                 result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result += sign * number;
+                number = 0;
 
-            } else if (ch == ')') {
-
-                // Evaluate the expression to the left
-                // with result, sign and operand
-                result += sign * operand;
-
-                // ')' marks end of expression within a set of parenthesis
-                // Its result is multiplied with sign on top of stack
-                // as stack.pop() is the sign before the parenthesis
                 result *= stack.pop();
-
-                // Then add to the next operand on the top.
-                // as stack.pop() is the result calculated before this parenthesis
-                // (operand on stack) + (sign on stack * (result from parenthesis))
                 result += stack.pop();
-
-                // Reset the operand
-                operand = 0;
             }
         }
-        return result + (sign * operand);
+        result += sign * number;
+        return result;
     }
 }
