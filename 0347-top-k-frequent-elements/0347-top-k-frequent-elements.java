26d344bb9b5c512c4c6b951bfb8a31a146ca0b46
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-05-25 16:05:35 +0530

    Time: 19 ms (9.89%), Space: 48 MB (13.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -6,31 +6,20 @@ class Solution {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        List<Integer>[] bucket = new List[nums.length + 1];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(map::get));
 
-        for (int key : map.keySet()) {
-            int freq = map.get(key);
-
-            if (bucket[freq] == null) {
-                bucket[freq] = new ArrayList<>();
-            }
-
-            bucket[freq].add(key);
-        }
-
-        int[] res = new int[k];
-        int idx = 0;
-
-        for (int i = bucket.length - 1; i >= 0 && idx < k; i--) {
-            if (bucket[i]  != null) {
-                for (int num : bucket[i]) {
-                    res[idx++] = num;
-
-                    if (idx == k)
-                        break;
-                }
+        for (int num : map.keySet()) {
+            pq.offer(num);
+            if (pq.size() > k) {
+                pq.poll();
             }
         }
-        return res;
+
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.poll();
+        }
+
+        return ans;
     }
 }
