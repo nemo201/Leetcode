ab2445a62e05cb617aaea75f64cdd33ca8b37af0
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-05-25 17:46:41 +0530

    Time: 1 ms (99.59%), Space: 42.6 MB (83.32%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,19 +1,19 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int s : stones)
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int s : stones) {
             pq.offer(s);
-        
-        int x, y;
-        while (pq.size() > 1) {
-            y = pq.poll();
-            x = pq.poll();
-            
-            if (y > x)
-                pq.offer(y - x);
         }
-        if (pq.size() == 0)
-            return 0;
-        return pq.poll();
+
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+
+            if (a != b) {
+                pq.offer(a - b);
+            }
+        }
+        return pq.size() == 1 ? pq.poll() : 0;
     }
 }
