c82d961b42d85896adab804eed1c040d0d89c633
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-05-26 12:56:35 +0530

    Time: 8 ms (25.32%), Space: 47.2 MB (46.49%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            adj.put(i, new ArrayList<>());
+        }
+
+        for (int[] pre : prerequisites) {
+            adj.get(pre[0]).add(pre[1]);
+            indegree[pre[1]]++;
+        }
+
+        boolean[] visited = new boolean[numCourses];
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+                visited[i] = true;
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            for (int nei : adj.get(course)) {
+                indegree[nei]--;
+
+                if (indegree[nei] == 0) {
+                    q.offer(nei);
+                    visited[nei] = true;
+                }
+            }
+        }
+
+        for (boolean v: visited) {
+            if (!v) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
