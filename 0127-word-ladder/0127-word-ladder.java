3a7ff642dcb86f06bf7f7ec2056303e9ee61aa05
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-05-27 15:25:10 +0530

    Time: 159 ms (19.99%), Space: 47.6 MB (85.4%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,37 +1,47 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        //create a set
         Set<String> dict = new HashSet<>(wordList);
+        //check if end word is in set
         if (!dict.contains(endWord)) {
             return 0;
         }
+
+        //create a q and add the begining word
         Queue<String> q = new LinkedList<>();
         q.offer(beginWord);
+        
+        //create a level(counter) to keep track of number of changes
         int level = 1;
-
+        //while q is not empty
         while (!q.isEmpty()) {
             int size = q.size();
+            //check if the q has the endword if yes then return level
             for (int i = 0; i < size; i++) {
-                String word = q.poll();
-                if (word.equals(endWord)) {
+                String cur = q.poll();
+
+                if (cur.equals(endWord)) {
                     return level;
                 }
-                char[] arr = word.toCharArray();
-                for (int j = 0; j < arr.length; j++) {
-                    char original = arr[j];
+                //convert the word into character array and check by replacing ever character if new word is in set
+                char[] word = cur.toCharArray();
+                for (int j = 0; j < word.length; j++) {
+                    char og = word[j];
                     for (char c = 'a'; c <= 'z'; c++) {
-                        arr[j] = c;
-                        String next = new String(arr);
-
+                        word[j] = c;
+        //if in set then add new word to q and remove from set
+                        String next = new String(word);
                         if (dict.contains(next)) {
                             q.offer(next);
                             dict.remove(next);
                         }
                     }
-                    arr[j] = original;
+        //backtrack to original word
+                    word[j] = og;
                 }
             }
             level++;
         }
-        return 0;
+        return 0;        
     }
 }
