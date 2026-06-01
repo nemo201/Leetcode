c10b951161fe425ab9556813b20fac6c5a23da7a
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-06-01 13:18:24 +0530

    Time: 661 ms (12.42%), Space: 166.8 MB (11.47%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,104 +1,76 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        
-        Queue<Pair<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a.getKey(), b.getKey()));
-        boolean[] visited = new boolean[n];
-        
-        minHeap.add(new Pair(0, 0));
-        int cost = 0;
+        List<int[]> edges = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int cost = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                edges.add(new int[] {cost, i, j});
+            }
+        }
+
+        Collections.sort(edges, (a, b) -> a[0] - b[0]);
+
+        UnionFind uf = new UnionFind(n);
+        int totalCost = 0;
         int edgesUsed = 0;
-        
-        while (edgesUsed < n) {
-            Pair<Integer, Integer> cell = minHeap.poll();
-            int weight = cell.getKey();
-            int curNode = cell.getValue();
-            
-            if (visited[curNode])
-                continue;
-            
-            visited[curNode] = true;
-            cost += weight;
-            edgesUsed++;
-            
-            for (int nextNode = 0; nextNode < n; nextNode++) {
-                if (!visited[nextNode]) {
-                    int nextWeight = Math.abs(points[curNode][0] - points[nextNode][0]) + Math.abs(points[curNode][1] - points[nextNode][1]);
-                    minHeap.add(new Pair(nextWeight, nextNode));
+
+        for (int[] edge : edges) {
+            int cost = edge[0];
+            int u = edge[1];
+            int v = edge[2];
+
+            if (uf.union(u, v)) {
+                totalCost += cost;
+                edgesUsed++;
+
+                if (edgesUsed == n - 1) {
+                    break;
                 }
             }
         }
-        return cost;
+        return totalCost;
+    }
+
+    class UnionFind {
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public boolean union(int x, int y) {
+            int px = find(x);
+            int py = find(y);
+
+            if (px == py) {
+                return false;
+            }
+
+            if (rank[px] < rank[py]) {
+                parent[px] = py;
+            } else if (rank[px] > rank[py]) {
+                parent[py] = px;
+            } else {
+                parent[py] = px;
+                rank[px]++;
+            }
+            return true;
+        }
     }
 }
-// Kruskal
-// class Solution {
-    
-//     class UnionFind{
-//         int[] root;
-//         int[] rank;
-        
-//         public UnionFind(int size) {
-//             root = new int[size];
-//             rank = new int[size];
-            
-//             for (int i = 0; i < size; i++) {
-//                 root[i] = i;
-//                 rank[i] = 1;
-//             }
-//         }
-        
-//         public int find(int x) {
-//             if (x == root[x])
-//                 return x;
-//             return root[x] = find(root[x]);
-//         }
-        
-//         public boolean union(int x, int y) {
-//             int Rx = find(x);
-//             int Ry = find(y);
-            
-//             if (Rx == Ry)
-//                 return false;
-            
-//             if (rank[Rx] < rank[Ry])
-//                 root[Rx] = Ry;
-//             else {
-//                 root[Ry] = Rx;
-//                 rank[Rx]++;
-//             }
-//             return true;
-//         }
-//     }
-    
-//     public int minCostConnectPoints(int[][] points) {
-//         int n = points.length;
-//         List<int[]> allEdges = new ArrayList<>();
-        
-//         for (int i = 0; i < n; i++) {
-//             for (int j = i + 1; j < n; j++) {
-//                 int weight = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                
-//                 int[] curEdge = {weight, i, j};
-//                 allEdges.add(curEdge);
-//             }
-//         }
-//         Collections.sort(allEdges, (a, b) -> Integer.compare(a[0], b[0]));
-        
-//         UnionFind uf = new UnionFind(n);
-//         int cost = 0;
-//         int edgesUsed = 0;
-        
-//          for (int i = 0; i < allEdges.size() && edgesUsed < n - 1; i++) {
-//             int n1 = allEdges.get(i)[1];
-//             int n2 = allEdges.get(i)[2];
-//             int weight = allEdges.get(i)[0];
-            
-//             if (uf.union(n1, n2)){
-//                 cost += weight;
-//                 edgesUsed++;
-//             }
-//         }
-//         return cost;
-//     }
-// }
