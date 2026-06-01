7247827cc8e2daeb0688edde918397ce24c97ec2
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-06-01 13:29:08 +0530

    Time: 600 ms (20.52%), Space: 76.6 MB (59.42%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,76 +1,32 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        List<int[]> edges = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int cost = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                edges.add(new int[] {cost, i, j});
-            }
-        }
-
-        Collections.sort(edges, (a, b) -> a[0] - b[0]);
-
-        UnionFind uf = new UnionFind(n);
+        boolean[] visited = new boolean[n];
         int totalCost = 0;
-        int edgesUsed = 0;
+        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        pq.offer(new int[]{0, 0});
+        int edge = 0;
 
-        for (int[] edge : edges) {
-            int cost = edge[0];
-            int u = edge[1];
-            int v = edge[2];
+        while (!pq.isEmpty() && edge < n) {
+            int[] cur = pq.poll();
+            int cost = cur[0];
+            int node = cur[1];
 
-            if (uf.union(u, v)) {
-                totalCost += cost;
-                edgesUsed++;
+            if (visited[node]) {
+                continue;
+            }
 
-                if (edgesUsed == n - 1) {
-                    break;
+            totalCost += cost;
+            edge--;
+            visited[node] = true;
+
+            for (int next = 0; next < n; next++) {
+                if (!visited[next]) {
+                    int nextCost = Math.abs(points[node][0] - points[next][0]) + Math.abs(points[node][1] - points[next][1]);
+                    pq.offer(new int[]{nextCost, next});
                 }
             }
         }
         return totalCost;
-    }
-
-    class UnionFind {
-        int[] parent;
-        int[] rank;
-
-        public UnionFind(int n) {
-            parent = new int[n];
-            rank = new int[n];
-
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-                rank[i] = 1;
-            }
-        }
-
-        public int find(int x) {
-            if (parent[x] != x) {
-                parent[x] = find(parent[x]);
-            }
-            return parent[x];
-        }
-
-        public boolean union(int x, int y) {
-            int px = find(x);
-            int py = find(y);
-
-            if (px == py) {
-                return false;
-            }
-
-            if (rank[px] < rank[py]) {
-                parent[px] = py;
-            } else if (rank[px] > rank[py]) {
-                parent[py] = px;
-            } else {
-                parent[py] = px;
-                rank[px]++;
-            }
-            return true;
-        }
     }
 }
