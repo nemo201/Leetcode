0a1c1b85cdcb2b0d5c518068d76fec6ffb2b3c05
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-08-18 16:36:52 +0530

    Time: 16 ms (69.18%), Space: 46.2 MB (84.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,30 +1,18 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Integer[][] memo = new Integer[amount + 1][coins.length];
-        int ans = helper (coins, amount, coins.length - 1, memo);
-        return ans >= 1_000_000 ? -1 : ans;
-    }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
 
-    private int helper (int[] coins, int target, int i, Integer[][] memo) {
-        if (target == 0) {
-            return 0;
+        dp[0] = 0;
+
+        for (int a = 1; a <= amount; a++) {
+            for (int coin : coins) {
+                if (a >= coin) {
+                    dp[a] = Math.min(dp[a], 1 + dp[a - coin]);
+                }
+            }
         }
 
-        if (i < 0) {
-            return 1_000_000;
-        }
-
-        if (memo[target][i] != null) {
-            return memo[target][i];
-        }
-
-        int take = 1_000_000;
-        if (target >= coins[i]) {
-            take = 1 + helper (coins, target - coins[i], i, memo);
-        }
-
-        int skip = helper (coins, target, i - 1, memo);
-
-        return memo[target][i] = Math.min(take, skip);
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }
