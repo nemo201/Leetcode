532655febbe5ca860c809d6948d6fba12c0cf0e2
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-03 14:44:57 +0530

    Time: 42 ms (11%), Space: 47.2 MB (57.86%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == Integer.MAX_VALUE)
+                continue;
+            for (int j = i + 1; j <= i + nums[i] && j < n; j++) {
+                dp[j] = Math.min(dp[j], dp[i] + 1);
+            }
+        }
+        return dp[n - 1];
+    }
+}
