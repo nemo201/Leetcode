793e2f1cffd586769ec591880d7300c51cad69a8
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-03 14:30:08 +0530

    Time: 15 ms (60.13%), Space: 51.4 MB (55.92%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int child = 0;
+        int cookie = 0;
+
+        while (child < g.length && cookie < s.length) {
+            if (s[cookie] >= g[child]) {
+                child++;
+                cookie++;
+            } else {
+                cookie++;
+            }
+        }
+        return child;
+    }
+}
