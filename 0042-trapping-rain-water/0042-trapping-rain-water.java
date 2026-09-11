7c5c02d01c608ad02ddb4f6c08f6a41a47e39482
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-11 17:42:17 +0530

    Time: 1 ms (56.35%), Space: 48.7 MB (20.98%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,25 +1,28 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int[] leftHeight = new int[n];
-        int[] rightHeight = new int[n];
-        leftHeight[0] = height[0];
-        rightHeight[n - 1] = height[n - 1];
+        
+        int[] leftmax = new int[n];
+        int[] rightmax = new int[n];
+
+        leftmax[0] = height[0];
+        rightmax[n - 1] = height[n - 1];
 
         for (int i = 1; i < n; i++) {
-            leftHeight[i] = Math.max(height[i], leftHeight[i - 1]);
+            leftmax[i] = Math.max(leftmax[i - 1], height[i]);
         }
 
         for (int i = n - 2; i >= 0; i--) {
-            rightHeight[i] = Math.max(height[i], rightHeight[i + 1]); 
+            rightmax[i] = Math.max(rightmax[i + 1], height[i]);
         }
 
         int[] minHeight = new int[n];
         for (int i = 0; i < n; i++) {
-            minHeight[i] = Math.min(leftHeight[i], rightHeight[i]);
+            minHeight[i] = Math.min(leftmax[i], rightmax[i]);
         }
 
         int water = 0;
+
         for (int i = 0; i < n; i++) {
             if (height[i] < minHeight[i]) {
                 water += minHeight[i] - height[i];
