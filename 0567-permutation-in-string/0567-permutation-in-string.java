03aad7b5ae4350cbca230d968ebc02dac7ca04e0
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-15 16:49:28 +0530

    Time: 7 ms (67.16%), Space: 44.2 MB (48%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,30 +1,32 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int [] count = new int[26];
-
-        for (char c : s1.toCharArray()) {
-            count[c - 'a']++;
+        if (s1.length() > s2.length()) {
+            return false;
         }
 
-        int left = 0, right = 0, required = s1.length();
+        int[] need = new int[26];
+        int[] window = new int[26];
 
-        while (right < s2.length()) {
-            if (count[s2.charAt(right) - 'a'] > 0) {
-                required--;
-            }
-            count[s2.charAt(right) - 'a']--;
+        for (char c : s1.toCharArray()) {
+            need[c - 'a']++;
+        }
 
-            right++;
-            if (required == 0) {
+        int k = s1.length();
+        for (int i = 0; i < k; i++) {
+            window[s2.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(need, window)) {
+            return true;
+        }
+
+        for (int right = k; right < s2.length(); right++) {
+            window[s2.charAt(right) - 'a']++;
+            int left = right - k;
+            window[s2.charAt(left) - 'a']--;
+
+            if (Arrays.equals(need, window)) {
                 return true;
-            }
-
-            if (right - left == s1.length()) {
-                if (count[s2.charAt(left) - 'a'] >= 0) {
-                    required++;
-                }
-                count[s2.charAt(left) - 'a']++;
-                left++;
             }
         }
         return false;
