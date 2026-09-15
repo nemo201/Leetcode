8f5cde587866be4f1314116e42be62fcd3059a54
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-15 15:55:50 +0530

    Time: 3 ms (71.72%), Space: 45.6 MB (69.49%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,6 +1,9 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (t.length() == 0) return "";
+
+        if (s.length() < t.length()) {
+            return "";
+        }
 
         int[] need = new int[128];
         int[] window = new int[128];
@@ -10,25 +13,27 @@ class Solution {
         }
 
         int required = 0;
+
         for (int i = 0; i < 128; i++) {
-            if (need[i] > 0) required++;
+            if (need[i] > 0) {
+                required++;
+            }
         }
 
         int formed = 0;
-        int left = 0, right = 0;
-        int minLen = Integer.MAX_VALUE, start = 0;
+        int left = 0, right = 0, start = 0;
+        int minLength = Integer.MAX_VALUE;
 
         while (right < s.length()) {
             char c = s.charAt(right);
             window[c]++;
-
             if (need[c] > 0 && window[c] == need[c]) {
                 formed++;
             }
 
             while (formed == required) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
                     start = left;
                 }
 
@@ -38,13 +43,10 @@ class Solution {
                 if (need[lc] > 0 && window[lc] < need[lc]) {
                     formed--;
                 }
-
                 left++;
             }
-
             right++;
         }
-
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        return minLength == Integer.MAX_VALUE ? "" : s.substring(start, start + minLength);
     }
 }
