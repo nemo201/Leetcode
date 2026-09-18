047d1790ae729566336a65e6204791863dad9caa
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-18 23:36:52 +0530

    Time: 0 ms (100%), Space: 43.7 MB (21.07%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -13,10 +13,12 @@ class Solution {
         if (head == null || head.next == null) {
             return null;
         }
+
         ListNode dummy = new ListNode(0);
         dummy.next = head;
 
-        ListNode slow = dummy, fast = dummy;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
 
         for (int i = 0; i <= n; i++) {
             fast = fast.next;
@@ -28,6 +30,7 @@ class Solution {
         }
 
         slow.next = slow.next.next;
+
         return dummy.next;
     }
 }
