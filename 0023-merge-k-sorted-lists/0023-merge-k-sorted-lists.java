8be9b2a5fb5f0371e534f85cd95a70f50de914b9
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-19 13:44:29 +0530

    Time: 2 ms (88.99%), Space: 47.1 MB (14.28%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -30,26 +30,25 @@ class Solution {
 
     private ListNode merge(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
+        ListNode next = dummy;
 
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
-                tail.next = l1;
+                next.next = l1;
                 l1 = l1.next;
             } else {
-                tail.next = l2;
+                next.next = l2;
                 l2 = l2.next;
             }
-
-            tail = tail.next;
+            next = next.next;
         }
 
         if (l1 != null) {
-            tail.next = l1;
+            next.next = l1;
         }
 
         if (l2 != null) {
-            tail.next = l2;
+            next.next = l2;
         }
 
         return dummy.next;
