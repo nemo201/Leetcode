35788202e7c22233514c066523b7ab710d75c1f9
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-20 13:32:09 +0530

    Time: 9 ms (95.89%), Space: 59.3 MB (61.99%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -31,30 +31,33 @@ class Solution {
             slow = slow.next;
             fast = fast.next.next;
         }
+
         return slow;
     }
 
     private ListNode merge(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
+        ListNode next = dummy;
 
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
-                tail.next = l1;
+                next.next = l1;
                 l1 = l1.next;
             } else {
-                tail.next = l2;
+                next.next = l2;
                 l2 = l2.next;
             }
-            tail = tail.next;
+            next = next.next;
         }
+
         if (l1 != null) {
-            tail.next = l1;
+            next.next = l1;
         }
 
         if (l2 != null) {
-            tail.next = l2;
+            next.next = l2;
         }
+
         return dummy.next;
     }
 }
