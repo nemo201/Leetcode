0a8f9ba6e87ee2a17dbb4e4a12937c8e13fb75f3
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-21 15:38:53 +0530

    Time: 3 ms (5.34%), Space: 48.2 MB (33.06%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,33 +1,25 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        
-        int[] leftmax = new int[n];
-        int[] rightmax = new int[n];
-
-        leftmax[0] = height[0];
-        rightmax[n - 1] = height[n - 1];
-
-        for (int i = 1; i < n; i++) {
-            leftmax[i] = Math.max(leftmax[i - 1], height[i]);
-        }
-
-        for (int i = n - 2; i >= 0; i--) {
-            rightmax[i] = Math.max(rightmax[i + 1], height[i]);
-        }
-
-        int[] minHeight = new int[n];
-        for (int i = 0; i < n; i++) {
-            minHeight[i] = Math.min(leftmax[i], rightmax[i]);
-        }
-
+        Deque<Integer> stack = new ArrayDeque<>();
         int water = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (height[i] < minHeight[i]) {
-                water += minHeight[i] - height[i];
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                int bottom = stack.pop();
+
+                if (stack.isEmpty()) {
+                    break;
+                }
+
+                int left = stack.peek();
+                int width = i - left - 1;
+                int boundedHeight = Math.min(height[left], height[i]) - height[bottom];
+
+                water += width * boundedHeight;
             }
+            stack.push(i);
         }
+
         return water;
     }
 }
