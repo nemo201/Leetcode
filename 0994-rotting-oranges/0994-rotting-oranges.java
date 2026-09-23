0cb77732bc3d3a5c2f50fb97c3bec7535ac0f915
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-23 13:12:19 +0530

    Time: 2 ms (87.49%), Space: 44.3 MB (39.56%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,14 +2,13 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-
-        int fresh = 0;
         Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2) {
-                    q.offer(new int[]{i, j});
+                    q.offer(new int[] { i, j });
                 }
                 if (grid[i][j] == 1) {
                     fresh++;
@@ -21,21 +20,22 @@ class Solution {
             return 0;
         }
 
+        int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
         int min = 0;
-        int[][] dirs = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+
         while (!q.isEmpty()) {
             int size = q.size();
             boolean rotted = false;
-            for (int i = 0; i < size; i++) {
-                int[] cur = q.poll();
 
+            for (int i = 0; i < size; i++) {
+                int[] node = q.poll();
                 for (int[] dir : dirs) {
-                    int nr = cur[0] + dir[0];
-                    int nc = cur[1] + dir[1];
+                    int nr = node[0] + dir[0];
+                    int nc = node[1] + dir[1];
 
                     if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1) {
-                        grid[nr][nc] = 2;
                         q.offer(new int[]{nr, nc});
+                        grid[nr][nc] = 2;
                         fresh--;
                         rotted = true;
                     }
