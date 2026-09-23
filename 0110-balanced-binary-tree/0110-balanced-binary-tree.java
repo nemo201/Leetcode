328dcc63075ef3b99879e2f72ba6a169878cf0d0
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-23 15:31:36 +0530

    Time: 0 ms (100%), Space: 45.6 MB (59.97%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,25 +15,26 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return helper(root) != -1;
+        return helper (root) != -1;
     }
 
-    private int helper(TreeNode node) {
+    private int helper (TreeNode node) {
         if (node == null) {
             return 0;
         }
 
-        int left = helper(node.left);
-
+        int left = helper (node.left);
         if (left == -1) {
             return -1;
         }
-        int right = helper(node.right);
+
+        int right = helper (node.right);
         if (right == -1) {
             return -1;
         }
+
         if (Math.abs(left - right) > 1) {
-            return -1;
+            return - 1;
         }
 
         return Math.max(left, right) + 1;
