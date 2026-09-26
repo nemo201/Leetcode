aeadde0bc1c4d98613e95ec0e59a91abd7daa0ed
Bishal Kumar Patel <62704202+nemo201@users.noreply.github.com>
2026-09-26 15:44:41 +0530

    Time: 15 ms (10.18%), Space: 47.2 MB (8.06%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,52 +1,43 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
         int n = edges.length;
 
-        UnionFind uf = new UnionFind(n + 1);
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
 
-        for (int[] edge : edges) {
-            if (uf.find(edge[0]) == uf.find(edge[1])) {
+        for (int []edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            boolean[] visited = new boolean[n + 1];
+
+            if (hasPath(adj, u, v, visited)) {
                 return edge;
             }
-            uf.union(edge[0], edge[1]);
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
 
-        return new int[]{};
+        return new int[0];
     }
 
-    class UnionFind {
-        int[] parent;
-        int[] rank;
-
-        public UnionFind(int n) {
-            parent = new int[n];
-            rank = new int[n];
-
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-                rank[i] = 1;
-            }
+    private boolean hasPath(List<List<Integer>> adj, int u, int v, boolean[] visited) {
+        if (u == v) {
+            return true;
         }
 
-        public int find(int x) {
-            if (parent[x] != x) {
-                parent[x] = find(parent[x]);
-            }
-            return parent[x];
-        }
+        visited[u] = true;
 
-        public void union(int x, int y) {
-            int px = find(x);
-            int py = find(y);
-
-            if (rank[px] < rank[py]) {
-                parent[px] = py;
-            } else if (rank[px] > rank[py]) {
-                parent[py] = px;
-            } else {
-                parent[px] = py;
-                rank[py]++;
+        for (int nei : adj.get(u)) {
+            if (!visited[nei]) {
+                if (hasPath(adj, nei, v, visited)) {
+                    return true;
+                }
             }
         }
+        return false;
     }
 }
